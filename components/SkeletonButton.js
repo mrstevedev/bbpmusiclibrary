8f79b659b351a3d785de1bd68ffcae02400cfbdd
Steven Polido <stevendotpulido@gmail.com>
2021-09-21 11:10:10 -0700

    Added <SkeletonButton /> component

--- components/SkeletonButton.js
+++ components/SkeletonButton.js
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton";
 
 const SkeletonButton = () => (
   <span className="skeleton-loading-container">
-    <Skeleton height={55} width={260}  />
+    <Skeleton height={55} width={270}  />
   </span>
 );
 
