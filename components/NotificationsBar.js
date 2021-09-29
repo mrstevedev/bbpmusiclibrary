5073c31223002475a1411e43e1f61e1c92a6a84d
Steven Polido <stevendotpulido@gmail.com>
2021-09-28 17:05:38 -0700

    Added <NotificationsBar /> component

---
+++ components/NotificationsBar.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from '../styles/NotificationsBar.module.scss'
+
+export default function NotificationsBar(props) {
+    return (
+        <>
+            <div className={ styles.NotificationsBar }>
+               <p></p>
+            </div>
+        </>
+    )
+}
