f9dfa37baccbd9b2a656f786bf72c71c8538972a
Steven Polido <stevendotpulido@gmail.com>
2021-09-30 21:26:05 -0700

    Added oAuth 1.0 flow to create WooCommerce order

--- server.js
+++ server.js
@@ -1,16 +1,115 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const axios = require('axios')
+const oauthSignature = require('oauth-signature')
 app.use(express.json())
 
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
-}))  
+}))
+
+function generateNonce() {
+    let text = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < 11; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text
+}
 
 app.post('/create-order', async (req, res) => {
-    const file = req.file
-    console.log('file:::::', file)
+    // const file = req.body.file
+    // const payment_method = req.body.payment_method
+
+    const body = req.body
+    const id = req.body.id
+    const name = req.body.description
+    const email_address = req.body.email_address
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const price = req.body.price
+    const admin_area_1 = req.body.admin_area_1
+    const admin_area_2 = req.body.admin_area_2
+    const address_line_1 = req.body.address_line_1
+    const postal_code = req.body.postal_code
+    const country_code = req.body.country_code
+    const phone = req.body.phone
+    const product_id = req.body.productId
+    const UnixTimestamp = Math.round(new Date().getTime() / 1000).toString()
+    
+    // Create an order with Woocommerce API orders endpoint
+
+    const json = {
+        "payment_method": body.payment_details.payment_method,
+        "payment_method_title": body.payment_details.payment_method,
+        "set_paid": true,
+        "status": "completed",
+        "billing": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "address_1": address_line_1,
+            "city": admin_area_2,
+            "state": admin_area_1,
+            "postcode": postal_code,
+            "country": country_code,
+            "email": email_address,
+            "phone": phone
+        },
+        "line_items": [
+            {
+            "product_id": product_id,
+            "quantity": 1,
+            "name": name,
+            "total": price
+            }
+        ],
+        "shipping_lines": [
+            {
+              "method_id": "flat_rate",
+              "method_title": "Flat Rate",
+              "total": "0"
+            }
+        ]
+    }
+
+    const httpMethod = 'POST',
+    url = 'http://localhost:10028/wp-json/wc/v3/orders',
+    requestParams = { 
+        oauth_consumer_key : 'twt881FSjosK',
+        oauth_token : 'l1mcMevgoJ6IaQbKPD0MDHDm',
+        oauth_nonce : generateNonce(),
+        oauth_timestamp : UnixTimestamp,
+        oauth_signature_method : 'HMAC-SHA1'
+    }
+
+    const consumerSecret = 'Hp89miPnVujATTgpZ7b8WflPuO3ss398NLfPRRGa37OFJBQF';
+    const tokenSecret = 'ihqFLs3hzAp4LTjPP3T7GbVpxpvxrUrxb81KdXN7d4ceLPRO';
+
+    const signature = oauthSignature.generate( httpMethod, url, requestParams, consumerSecret, tokenSecret, { 
+        encodeSignature: true } )
+
+    const authorizationHeader = 
+          'OAuth oauth_consumer_key="' + requestParams.oauth_consumer_key
+          + '",oauth_nonce="' + requestParams.oauth_nonce
+          + '",oauth_signature_method="' + requestParams.oauth_signature_method
+          + '",oauth_timestamp="' + requestParams.oauth_timestamp
+          + '",oauth_token="' + requestParams.oauth_token
+          + '",oauth_signature="' + signature + '"'
+
+    axios.post(url, json, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authorizationHeader
+        }
+    })
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => console.log(err))
+
+    // Email file with Nodemailer 
     res.send('ok')
 })
 
