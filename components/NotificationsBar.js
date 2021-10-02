314ed8ebc5c4dd33b51b5eba0db814a848a36719
Steven Polido <stevendotpulido@gmail.com>
2021-10-02 00:56:22 -0700

    Added global className

--- components/NotificationsBar.js
+++ components/NotificationsBar.js
@@ -4,7 +4,7 @@ import styles from '../styles/NotificationsBar.module.scss'
 export default function NotificationsBar(props) {
     return (
         <>
-            <div className={ styles.NotificationsBar }>
+            <div className={ `${ styles.NotificationsBar } Notifications__Mobile`}>
                <p className={ styles.NotificationsBar__text }>
                     Fall 2021 30% off sale. Coupon code added to order at checkout
                </p>
