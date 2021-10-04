bd841a4b003fc7c1e7daa402ac554f754fb0a5d2
Steven Polido <stevendotpulido@gmail.com>
2021-10-03 22:21:26 -0700

    Added Related Products slidehow

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,7 +16,7 @@ export default function Product(props) {
   const [gallery, showGallery] = useState(false);
   const [cart, setCart] = useContext(AppContext);
 
-  // console.log(data)
+  console.log(props)
 
   const handleAddToCart = () => {
     // const addToCartBtn = document.querySelector('.add-to-cart-btn ')
@@ -89,6 +89,20 @@ export default function Product(props) {
     });
     e.currentTarget.classList.add("active");
   };
+
+  const handleSlidePrev = () => {
+    console.log('Go to previous')
+    const productRelated = document.querySelector('.Product_product__related__2f5mX')
+    productRelated.scrollLeft -= 350;
+    
+
+  }
+
+  const handleSlideNext = () => {
+    console.log('Go to next slide')
+    const productRelated = document.querySelector('.Product_product__related__2f5mX')
+    productRelated.scrollLeft += 350;
+  }
 
   const { product } = props;
   const { name, description, price } = props.product;
@@ -247,6 +261,40 @@ export default function Product(props) {
               </p>
             </div>
           </div>
+           
+           <div className="related__custom" style={{
+                 position: 'relative'
+           }}>
+              <button onClick={handleSlidePrev} className="carousel__control carousel__control--prev" type="button" aria-describedby="carousel-status-s0-0-32-3-0-0[6]-4-match-media-0-ebay-carousel" aria-label="Go to previous slide - Samsung Cell Phones &amp; Smartphones" aria-disabled="true">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.2426 6.34317L14.8284 4.92896L7.75739 12L14.8285 19.0711L16.2427 17.6569L10.5858 12L16.2426 6.34317Z" fill="currentColor" /></svg>
+              </button>
+              <button onClick={handleSlideNext} className="carousel__control carousel__control--next" type="button" aria-describedby="carousel-status-s0-0-32-3-0-0[6]-4-match-media-0-ebay-carousel" aria-label="Go to next slide - Samsung Cell Phones &amp; Smartphones" id="s0-0-32-3-0-0[6]-4-match-media-0-ebay-carousel-next">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z" fill="currentColor"/></svg>
+              </button>
+          <div className={ styles.product__related }>
+              <h2 style={{ fontSize: '1rem' }}>Related Products</h2>
+              <div className={ styles.product__relatedGallery }>
+              { product.related.edges.map((item) => (
+                <div key={item.node.id} className={ styles.product__relatedItem }>
+                    <Link href={ `${ item.node.slug }` }>
+                      <a>
+                        <div className={styles.product__Img}>
+                          <Image src={item.node.image.mediaItemUrl} width="400" height="400" alt={`Gallery Image - ${ item.name }`} />
+                        </div>
+                      </a>
+                    </Link>
+                  <Link href={ `${ item.node.slug }` }>
+                    <a className="link">
+                      <p style={{ fontSize: '0.8rem', margin: '0.5rem 0 0 0' }}>{ item.node.name }</p>
+                    </a>
+                  </Link>
+                </div>
+              )) }
+              </div>
+
+            </div>
+            </div>
+
         </div>
         </div>
       </main>
@@ -256,7 +304,7 @@ export default function Product(props) {
 }
 
 export async function getStaticProps(context) {
-
+  console.log('context', context)
   const res = await fetch(process.env.SITE_URL, {
     method: "POST",
     headers: { "Content-type": "application/json" },
@@ -284,6 +332,18 @@ export async function getStaticProps(context) {
                 image {
                   mediaItemUrl
                   id
+                }
+                related {
+                  edges {
+                    node {
+                      id
+                      name
+                      slug
+                      image {
+                        mediaItemUrl
+                      }
+                    }
+                  }
                 }
               }
             }
