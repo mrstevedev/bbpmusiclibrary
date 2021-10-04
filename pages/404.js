9df0bcdde21dc2c5fae09687a099123a96b68954
Steven Polido <stevendotpulido@gmail.com>
2021-10-04 12:46:55 -0700

    Updated custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,22 @@
 // pages/404.js
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <div className="container" style={{
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '85vh',
+            display: 'flex'
+        }}>
+            <h1 style={{
+                    fontWeight: 'bold',
+                    fontSize: '1.1rem'
+            }}>404 <span style={{ padding: '0 0.3rem' }}>|</span></h1>
+            <h2 style={{
+                    fontWeight: 100,
+                    textTransform: 'uppercase',
+                    fontSize: '1rem'
+            }}>Page Not Found</h2>
+        </div>
+    )
   }
