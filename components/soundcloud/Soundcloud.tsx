60f0f872e6d077b7cc179e49570f9868dfd7a8b1
Steven Polido <stevendotpulido@gmail.com>
2021-10-08 12:19:10 -0700

    Updated to playlist

--- components/soundcloud/Soundcloud.tsx
+++ components/soundcloud/Soundcloud.tsx
@@ -3,7 +3,7 @@
 export default function Soundcloud() {
     return (
         <>
-         <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1109330596&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+         <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1304108839&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <div>
                 <a href="https://soundcloud.com/marioluciano425" title="Polyphonic Music Library" target="_blank" rel="noreferrer" style={{ color: "#333", fontWeight: "bold" }}>Polyphonic Music Library</a> · 
                 <a href="https://soundcloud.com/marioluciano425/1-soul-expressions-kit-preview" title="1.Soul Expressions (Kit Preview)" target="_blank" rel="noreferrer" style={{ color: "#333", fontWeight: "bold" }}>1.Soul Expressions (Kit Preview)</a>
