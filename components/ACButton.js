18d7dad59438a06b99a699ed47945376b256c026
Steven Polido <stevendotpulido@gmail.com>
2021-10-09 13:44:23 -0700

    Added check if a file is available for download

--- components/ACButton.js
+++ components/ACButton.js
@@ -3,11 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function ACButton(props) {
-    const { addToCart, product, sideBar, productPage } = props;
+    const { addToCart, product, productPage } = props;
+    const { downloads } = props.product;
+    console.log('downloads prop in ACButton', props )
 
   return (
     <>
-      { productPage ? (
+    { productPage && downloads !== null ? (
           <button className={`add-to-cart-btn ${ !addToCart ? styles.addToCartBtn : styles.viewCartBtn }`} onClick={ props.handleAddToCart }>
             { addToCart === true ? (
               <>
@@ -26,16 +28,25 @@ export default function ACButton(props) {
             )}
            
           </button>
-      ) : sideBar ? (
-        <Link href="/checkout">
-          <a>
-          <button className={`add-to-cart-btn ${ styles.addToCartBtn }`} onClick={props.handleCloseCart}>
-              <span className={styles.cartBtnTxt}>Checkout</span>
-              <Image src="/images/cart__haveItems.svg" width="24" height="22" alt="Checkout Item" />
-          </button>
-          </a>
-        </Link>
-      ) : ''  }
-    </>
+
+      ) : (
+        <>
+        { addToCart === true ? (
+          <>
+          <span className={styles.cartBtnTxt}>
+            Item added
+          </span>
+          <Image src="/images/shopping-cart_check_black.svg" width="30" height="28" alt="Add to Cart" />
+          </>
+        ) : (
+          <>
+          <span className={styles.cartBtnTxt} style={{ fontWeight: 'bold', color: '#a9a9a9' }}>
+            Not available. Please check back later
+          </span>
+          </>
+        )}
+        </>
+      )  }
+        </>
   );
 }
