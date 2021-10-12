497ddf1d2b57d0a2f83ba3c994818d676d349d18
Steven Polido <stevendotpulido@gmail.com>
2021-10-12 13:07:41 -0700

    Deleted SkeletonButton.js

--- components/SkeletonButton.js
+++
@@ -1,9 +0,0 @@
-import Skeleton from "react-loading-skeleton";
-
-const SkeletonButton = () => (
-  <span className="skeleton-loading-container">
-    <Skeleton height={55} width={270}  />
-  </span>
-);
-
-export default SkeletonButton;
