91942b8fb7ef490478b1dd903056074aea2e7180
Steven Polido <stevendotpulido@gmail.com>
2021-10-12 18:13:50 -0700

    Set fontWeight to bold

--- components/related/RelatedProducts.tsx
+++ components/related/RelatedProducts.tsx
@@ -50,7 +50,7 @@ export default function RelatedProducts( props: IProduct ) {
               <svg style={{ margin: "0 0.19rem 0 0" }} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z" fill="currentColor"/></svg>
               </button>
           <div className={ styles.product__related }>
-              <h2 style={{ fontSize: '1rem' }}>Related Products</h2>
+              <h2 style={{ fontSize: '1rem', fontWeight: 'bold' }}>Related Products</h2>
               <div className={ styles.product__relatedGallery }>
               { props.product.related.edges.map((item: IEdges) => (
                 <div key={item.node.id} className={ `product-img ${ styles.product__relatedItem } ` }>
