08c77bdbde53111c5df9988c2bff8666350db74a
Steven Polido <stevendotpulido@gmail.com>
2021-10-15 00:01:40 -0700

    Added prop to component

--- pages/about.tsx
+++ pages/about.tsx
@@ -37,7 +37,7 @@ export default function About({ page } : Props ) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={ styles.about__mainContainer }>
-        <ImageHero mediaItemUrl={page.featuredImage.node.mediaItemUrl} />
+        <ImageHero about mediaItemUrl={page.featuredImage.node.mediaItemUrl} />
         <div className="container">
           <div
             className={styles.about}>
