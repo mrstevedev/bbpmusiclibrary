413f0b0c8c5280c87e192e1a57c21a1facbc6051
Steven Polido <stevendotpulido@gmail.com>
2021-10-15 14:44:20 -0700

    Ternary conditional background styles depending on page

--- components/ImageHero.js
+++ components/ImageHero.js
@@ -4,9 +4,17 @@ export default function ImageHero(props) {
   console.log('props in hero image', props)
   return (
     <>
-      <div className={`hero-img ${styles.Bonita__Hero}`} style={{
-        background: `url(${ props.mediaItemUrl }) center -530px no-repeat`
-      }}></div>
+      { props.contact ? (
+        <div className={`hero-img ${styles.Bonita__Hero}`} style={{
+          background: `url(${ props.mediaItemUrl }) center -130px no-repeat`,
+          backgroundSize: 'cover'
+        }}></div>
+      ) : props.about ? (
+        <div className={`hero-img ${styles.Bonita__Hero}`} style={{
+          background: `url(${ props.mediaItemUrl }) center -530px no-repeat`,
+          // backgroundSize: 'cover'
+        }}></div>
+      ) : '' }
     </>
   );
 }
