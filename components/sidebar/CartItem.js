11a71f40d244a75f99f31b9aa6b290a8bf103270
Steven Polido <stevendotpulido@gmail.com>
2021-10-21 18:24:32 -0700

    Output data passed in as props

---
+++ components/sidebar/CartItem.js
@@ -0,0 +1,37 @@
+import styles from '../../styles/SidebarCart.module.scss'
+import { Fragment } from 'react'
+import Link from 'next/link'
+import Image from "next/image";
+
+export default function CartItem(props) {
+
+    const { product, productsCount } = props
+
+    return (
+        <>
+            <div className={styles.Checkout__product}>
+                <div className={styles.Checkout_topImg}>
+                    <Link href={`/product/${ product.slug }`}>
+                        <a className="product-img">
+                            <span className={`cart-count ${ styles.cart__count }`}>{ productsCount }</span>
+                            <Image src={ product.image } width="81" height="81" alt="Cart items" />
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles.Checkout__right}>
+                    <h3>
+                        <Link href={` /product/${ product.slug } `}>
+                            <a>
+                            { product.name }
+                            {/* { products[0] !== undefined ? products[0].name : "" } */}
+                            </a>
+                        </Link>
+                        <span className={styles.Checkout__right_price}>${ product.price }</span>
+                        </h3>
+                    
+                    <a onClick={() => handleRemoveItem(product.productId)} href="#" className={styles.Checkout__right_btn}>Remove</a>
+                </div>
+            </div>
+        </>
+    )
+}
