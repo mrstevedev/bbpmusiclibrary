4934cf8972eca308c77a4aa3c94cab0ceab401a7
Steven Polido <stevendotpulido@gmail.com>
2021-10-25 16:27:43 -0700

    Added <SidebarCart /> component

---
+++ components/checkout/SidebarCart.tsx
@@ -0,0 +1,48 @@
+
+import styles from '../../styles/Checkout.module.scss'
+import CartItem from '../../components/checkout/CartItem'
+
+interface Product {
+    databaseId: string,
+    name: string,
+    image: string
+    price: number,
+    slug: string
+}
+
+interface IProps {
+    products: Product[]
+    productsCount: number
+    price: number
+}
+
+export default function SidebarCart(props : IProps) {
+    const { products, productsCount, price } = props
+
+    return (
+        <>
+            <div className={styles.Checkout__right}>
+                <div className={styles.Checkout__right_top}>
+                    <h3 className={styles.Checkout__right_topTitle}>Checkout</h3>
+                </div>
+                <div className={styles.Checkout__right_btm}>
+
+                    <CartItem 
+                        price={price}
+                        products={products} 
+                        productsCount={productsCount} 
+                    />
+
+                </div>
+                <div className={styles.Checkout__right_btm_total_container}>
+                    <div className={styles.Checkout__right_btm_total}>
+                    <h4>Total</h4>
+                    </div>
+                    <div>
+                    <span className="Checkout_currency__ticker">USD</span> { price }
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
