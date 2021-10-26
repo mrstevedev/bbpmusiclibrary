bff4aca99b975a7e7944b235fa1b5ba70bfaf004
Steven Polido <stevendotpulido@gmail.com>
2021-10-26 14:09:30 -0700

    Added <RelatedItem /> component

---
+++ components/related/RelatedItem.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/Related.module.scss";
+
+interface IEdges {
+    node: {
+        id: string
+        image: {
+            mediaItemUrl: string    
+        }
+        name: string
+        slug: string
+    }
+}
+
+interface ICategories {
+
+}
+
+interface IProduct {
+    product: {
+        related: {
+            edges: IEdges[]
+        }
+    }
+}
+
+export default function RelatedItem(props: IProduct) {
+    console.log(props)
+    return (
+        <>
+            { props.product.related.edges.map((item: IEdges) => (
+                <div key={item.node.id} className={ `product-img ${ styles.product__relatedItem } ` }>
+                    <Link href={ `${ item.node.slug }` }>
+                      <a>
+                        <div className={styles.product__Img}>
+                          <Image src={item.node.image !== null ? item.node.image.mediaItemUrl : 'http://localhost:10028/wp-content/uploads/2021/10/websiteplanet-dummy-640X640.webp' } width="400" height="400" alt={`Bonita Basics Productions Music Library - ${ item.node.name }`} />
+                          
+                        </div>
+                      </a>
+                    </Link>
+                  <Link href={ `${ item.node.slug }` }>
+                    <a className="link">
+                      <p style={{ fontSize: '0.8rem', margin: '0.5rem 0 0 0' }}>{ item.node.name }</p>
+                    </a>
+                  </Link>
+                </div>
+              )) }
+        </>
+    )
+}
