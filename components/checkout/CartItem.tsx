677b656b427087d37d03bf3e45d33b1d65cbc288
Steven Polido <stevendotpulido@gmail.com>
2021-10-25 17:41:32 -0700

    Refactored child components interface and props

--- components/checkout/CartItem.tsx
+++ components/checkout/CartItem.tsx
@@ -2,50 +2,45 @@ import styles from '../../styles/Checkout.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
-interface Product {
-    databaseId: string,
-    name: string,
-    image: string
-    price: number,
-    slug: string
-}
-
 interface IProps {
-    products: Product[]
+    product: {
+        databaseId: string,
+        name: string,
+        image: string
+        price: number,
+        slug: string
+    }
     productsCount: number
     price: number
 }
 
 export default function CartItem(props : IProps) {
 
-    const { products, productsCount } = props
+    const { productsCount } = props
+    const { databaseId, slug, image, name, price } = props.product
 
     return (
         <>
-            {products ?
-                products.map((product: Product) => (
-                    <div key={ product.databaseId } className={styles.Checkout__right_product}>
-                    <div className={`product-img ${styles.Checkout__right_product_img}`}>
-                    <Link href={`/product/${ product.slug }`}>
-                        <a>
-                        <span className="cart-count">{ productsCount }</span>
-                        <Image src={product.image} width="91" height="91" alt={ product.name } />
+            <div key={ databaseId } className={styles.Checkout__right_product}>
+                <div className={`product-img ${styles.Checkout__right_product_img}`}>
+                <Link href={`/product/${ slug }`}>
+                    <a>
+                    <span className="cart-count">{ productsCount }</span>
+                    <Image src={image} width="91" height="91" alt={ name } />
+                    </a>
+                    </Link>
+                    </div>
+                    <div className={styles.Checkout__right_product_name}>
+                    <h3 className={styles.Checkout__right_product_name_txt}>
+                        <Link href={`/product/${ slug }`}>
+                        <a style={{ color: '#333', fontWeight: 100 }}>
+                            { name }
                         </a>
                         </Link>
-                        </div>
-                        <div className={styles.Checkout__right_product_name}>
-                        <h3 className={styles.Checkout__right_product_name_txt}>
-                            <Link href={`/product/${ product.slug }`}>
-                            <a style={{ color: '#333', fontWeight: 100 }}>
-                                { product.name }
-                            </a>
-                            </Link>
-                        </h3>
-                        </div>
-                    <span>{ product.price }</span>
-                </div>
-            ))
-            : 'There are no items in your cart'}
+                    </h3>
+                    </div>
+                <span>{ price }</span>
+            </div>
         </>
     )
 }
