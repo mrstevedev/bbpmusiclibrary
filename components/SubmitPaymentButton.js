aac0c81fcbb3b1dfbdd073689e63afc51df6c206
Steven Polido <stevendotpulido@gmail.com>
2021-11-08 13:28:06 -0800

    Removed comment

--- components/SubmitPaymentButton.js
+++ components/SubmitPaymentButton.js
@@ -6,7 +6,6 @@ export default function SubmitPaymentButton(props) {
     <>
       <button type="submit" className="btn btn-primary btn-block p-3" onClick={props.handleSubmitBtn}>
         <span style={{ fontSize: '0.9rem' }}>{props.formSubmit ? <Spinner /> : 'Continue to payment' }</span>
-        {/* <Image src={paypalIcon} width="22" height="20" /> */}
       </button>
     </>
   );
