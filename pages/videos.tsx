de02b5c65f1ce219e9ed6b91fd39a22990a9548f
Steven Polido <stevendotpulido@gmail.com>
2021-11-11 10:51:40 -0800

    Replaced inline cover image with <CoverImage /> component

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -1,14 +1,11 @@
-
+import styles from '../styles/CoverImage.module.scss'
 
 export default function Videos() {
     return (
         <> 
-         <div className="hero-img" style={{ backgroundImage: `url(${'./images/img1200.webp'})`, 
-          height: '270px',
-          width: '100%',
-          backgroundPosition: 'center top',
-          backgroundRepeat: 'no-repeat',
-          margin: '2rem 0 2rem 0', borderRadius: '3px' }}></div>
+         <div className={`hero-img ${ styles.cover__image }`} style={{ 
+             backgroundImage: `url(${'./images/img1200.webp'})`, 
+         }}></div>
             <div className="container">
                 <h1 style={{ fontWeight: 100, margin: '3rem 0 2rem 0', fontSize: '0.7rem', textTransform: 'uppercase' }}>Music Videos</h1>
 
