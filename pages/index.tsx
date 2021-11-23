4de6a6fcfe754f0f3801efcd80b326b70cd1e8fe
Steven Polido <stevendotpulido@gmail.com>
2021-11-23 11:57:55 -0800

    Removed markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,12 +52,7 @@ export default function Home( { products } : Props ) {
 
         <div className="container">
 
-          <h1 style={{ fontWeight: 100, margin: '3rem 0 2rem 0', fontSize: '0.7rem', textTransform: 'uppercase' }}>Latest Sample Pack Releases 
-            <span style={{ color: '#939393' }}> Showing { products.length }</span></h1>
-
-          <Products 
-            products={products}
-          />
+          <Products products={products} />
 
         </div>
       </main>
