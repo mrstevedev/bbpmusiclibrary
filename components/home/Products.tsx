fdc7ccf47ab9397f05d57b8f68113f7bda9b1901
Steven Polido <stevendotpulido@gmail.com>
2021-11-27 19:57:24 -0800

    Decreased margin

--- components/home/Products.tsx
+++ components/home/Products.tsx
@@ -23,7 +23,7 @@ export default function Products({ products } : IProducts) {
     return (
         <>
           <div className="row products">
-          <h1 style={{ fontWeight: 100, margin: '3rem 0 2rem 0', fontSize: '0.7rem', textTransform: 'uppercase' }}>
+          <h1 style={{ fontWeight: 100, margin: '0 0 2rem 0', fontSize: '0.7rem', textTransform: 'uppercase' }}>
             Latest Sample Pack Releases 
             <span style={{ color: '#939393' }}> Showing { products.length }</span></h1>
             {products.map((product) => {
