e6ae5bef6ed8bcc4002c0a4bfe8135cf78499778
Steven Polido <stevendotpulido@gmail.com>
2021-12-11 10:04:07 -0800

    Added extra space

--- components/soundcloud/Soundcloud.tsx
+++ components/soundcloud/Soundcloud.tsx
@@ -48,7 +48,7 @@ export default function Soundcloud(props: IProduct) {
          <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src={ `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${ sku }&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true` }></iframe>
             <div>
                 <a href="https://soundcloud.com/marioluciano425" title="Polyphonic Music Library" target="_blank" rel="noreferrer" style={{ color: "#333", fontWeight: "bold" }}>Polyphonic Music Library</a> · 
-                <a href={ shortDescription } title="1.Soul Expressions (Kit Preview)" target="_blank" rel="noreferrer" style={{ color: "#333", fontWeight: "bold" }}>{ name }</a>
+                <a href={ shortDescription } title="1.Soul Expressions (Kit Preview)" target="_blank" rel="noreferrer" style={{ color: "#333", fontWeight: "bold" }}> { name }</a>
             </div>
         </>
     )
