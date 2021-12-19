81aa22e7dbd9451aea714bb804a9beaabaf17de9
Steven Polido <stevendotpulido@gmail.com>
2021-12-19 09:03:19 -0800

    Added generatePassword fn

--- util/generate.js
+++ util/generate.js
@@ -11,7 +11,20 @@ function generateNonce() {
     return text
 }
 
+function generatePassword() {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const passwordLength = 12
+    let password = ""
+
+    for (let i = 0; i <= passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber +1)
+    }
+    return password
+}
+
 module.exports = {
+    generatePassword,
     generateTimestamp,
     generateNonce
 }
