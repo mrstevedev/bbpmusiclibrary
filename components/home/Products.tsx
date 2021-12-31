9c473b321cf4310af200f67e41b658b97f17dfc4
Steven Polido <stevendotpulido@gmail.com>
2021-12-31 11:56:58 -0800

    Removed inline styles

--- components/home/Products.tsx
+++ components/home/Products.tsx
@@ -1,3 +1,4 @@
+import styles from '../../styles/Products.module.scss'
 import ProductItem from './ProductItem'
 
 interface IProduct {
@@ -22,10 +23,10 @@ export default function Products({ products } : IProducts) {
 
     return (
         <>
-          <div className="row products">
-          <h1 style={{ fontWeight: 100, margin: '0 0 2rem 0', fontSize: '0.7rem', textTransform: 'uppercase' }}>
+          <div className={`row ${ styles.products }`}>
+            <h1>
             Latest Sample Pack Releases 
-            <span style={{ color: '#939393' }}> Showing { products.length }</span></h1>
+            <span> Showing { products.length }</span></h1>
             {products.map((product) => {
                 return (
                     <ProductItem key={product.node.id} product={product} />
