29f6085d1dbf1bd90be8a664dc7e7a50a3185ee6
Steven Polido <stevendotpulido@gmail.com>
2022-01-09 11:08:28 -0800

    Removed unused style attribute

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,7 @@ import styles from '../styles/Error_404.module.scss'
 
 export default function Custom404() {
     return (
-        <div className={`container ${ styles.Error__404 }`} style={{
-           
-        }}>
+        <div className={`container ${ styles.Error__404 }`}>
             <h1 className={styles.Error__404__heading}>404 <span>|</span></h1>
             <h2 className={styles.Error__404__Subheading}>Page Not Found</h2>
         </div>
