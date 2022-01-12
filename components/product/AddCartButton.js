9feec17bdc4e4a697f33dfe33827fd28f985940e
Steven Polido <stevendotpulido@gmail.com>
2022-01-11 17:49:01 -0800

    Renamed component

---
+++ components/product/AddCartButton.js
@@ -0,0 +1,43 @@
+import styles from '../../styles/AddCartButton.module.scss'
+import Link from 'next/link'
+
+export default function ACButton(props) {
+    const { addToCart, product, productPage } = props;
+    const { downloads } = props.product;
+
+  return (
+    <>
+    { downloads !== null ? (
+            <>
+            { addToCart === true ? (
+              <>
+                <Link href="/cart">
+                  <a>
+                    <button className={`add-to-cart-btn ${ !addToCart ? styles.addToCartBtn : styles.viewCartBtn }`}>
+                      <span className={styles.cartBtnTxt}>
+                        View cart
+                      </span>
+                    </button>
+                  </a>
+                </Link>
+              </>
+            ) : (
+              <>
+              <button className={`add-to-cart-btn ${ !addToCart ? styles.addToCartBtn : styles.viewCartBtn }`} onClick={ props.handleAddToCart }>
+                <span className={styles.cartBtnTxt}>
+                  Add to cart
+                </span>
+              </button>
+              </>
+            )}
+          </>
+      ) : (
+        <>
+          <span className={styles.cartBtnTxt__notAvailable}>
+            Not available. Please check back later
+          </span>
+        </>
+      )  }
+      </>
+  );
+}
