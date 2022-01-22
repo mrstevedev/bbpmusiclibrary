75f1fc8497e67b35d055e41e4014ea7e5c3c2511
Steven Polido <stevendotpulido@gmail.com>
2022-01-22 15:22:34 -0800

    Removed unneeded imports

--- components/checkout/ExpressCheckout.tsx
+++ components/checkout/ExpressCheckout.tsx
@@ -7,13 +7,6 @@ import GooglePayButton from "@google-pay/button-react"
 import { formatPhoneNumber } from "../../util";
 import Router from 'next/router'
 
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-
-// Make sure to call `loadStripe` outside of a component's render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
-
 interface IProps {
     loading: boolean
     price: number
