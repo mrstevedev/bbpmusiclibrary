5a764ac919b37f9878aefeba31477ba4d2d83dbd
Steven Polido <stevendotpulido@gmail.com>
2022-05-14 18:58:39 -0700

    Prevent adding duplicate product to cart

--- components/product/AddCartButton.js
+++ components/product/AddCartButton.js
@@ -1,9 +1,16 @@
 import styles from '../../styles/AddCartButton.module.scss'
 import Link from 'next/link'
+import { AppContext } from "../../components/context/AppContext";
+import { useContext } from "react";
 
 export default function ACButton(props) {
+    const [cart, setCart] = useContext(AppContext);
     const { addToCart, product, productPage } = props;
     const { downloads } = props.product;
+    
+    // Check if current product databaseId matches the databaseId of a product in the context
+    // console.log(product.databaseId);
+    const hasItemInCart = cart?.products?.some(prod => prod.databaseId === product.databaseId);
 
   return (
     <>
@@ -20,6 +27,14 @@ export default function ACButton(props) {
                     </button>
                   </a>
                 </Link>
+              </>
+            ) : hasItemInCart ? (
+              <>
+              <button className={`add-to-cart-btn ${ !addToCart ? styles.addToCartBtn : styles.viewCartBtn }`}>
+                <span className={styles.cartBtnTxt}>
+                  Item is already in your cart
+                </span>
+              </button>
               </>
             ) : (
               <>
