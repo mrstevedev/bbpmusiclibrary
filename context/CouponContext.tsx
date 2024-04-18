084c646f70a3549dc433ae69ff683758f88793f3
Steven <stevendotpulido@gmail.com>
2024-04-17 21:41:21 -0700

    Add CouponContext for global coupons

---
+++ context/CouponContext.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, createContext } from "react";
+
+export type TCouponContext = {
+  couponValue: string;
+  setCouponValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const CouponContext = createContext<TCouponContext>({
+  couponValue: "",
+  setCouponValue: () => {},
+});
+
+type CouponProps = {
+  children: object;
+};
+
+export const CouponProvider = ({ children }: CouponProps) => {
+  const [couponValue, setCouponValue] = useState("");
+
+  useEffect(() => {
+    if (process.browser) {
+      const cart = localStorage.getItem("bbp_product") as string;
+      const parsed = JSON.parse(cart);
+      if (parsed) {
+        const coupon = parsed.coupon.code;
+        setCouponValue(coupon);
+      }
+    }
+  }, []);
+
+  return (
+    <CouponContext.Provider value={{ couponValue, setCouponValue }}>
+      {children}
+    </CouponContext.Provider>
+  );
+};
