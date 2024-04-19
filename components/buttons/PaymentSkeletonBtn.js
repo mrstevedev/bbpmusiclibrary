cfb505b104bab6693052333b754382401a0f746b
Steven <stevendotpulido@gmail.com>
2024-04-18 18:55:32 -0700

    Remove PaymentSkeletonBtn.js component

--- components/buttons/PaymentSkeletonBtn.js
+++
@@ -1,9 +0,0 @@
-import Skeleton from "react-loading-skeleton";
-
-const PaymentSkeletonBtn = () => (
-  <span className="skeleton-loading-container">
-    <Skeleton height={55} width={270}  />
-  </span>
-);
-
-export default PaymentSkeletonBtn;
