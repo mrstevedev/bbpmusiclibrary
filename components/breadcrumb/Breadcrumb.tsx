61652aa6b35cb1b5b7e4b022905507bbb6e3ec87
Steven <stevendotpulido@gmail.com>
2024-04-19 16:31:01 -0700

    Add Breadcrumb.tsx component

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -1,32 +1,54 @@
 import Link from "next/link";
+import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { BreadcrumbItem } from "react-bootstrap";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 
-interface IProduct {
-    product: {
-        name: string
-    }
-}
+type TBreadcrumbProps = {
+  slug: string;
+  homeElement: ReactNode;
+  containerClasses?: string;
+  listClasses?: string;
+  activeClasses?: string;
+  capitalizeLinks?: boolean;
+};
 
-export default function Breadcrumb(props: IProduct) {
+export default function BreadcrumbComponent({
+  slug,
+  homeElement,
+  activeClasses,
+  listClasses,
+  capitalizeLinks,
+}: TBreadcrumbProps) {
+  const router = useRouter();
+  const paths = router.asPath;
+  const pathNames = paths.split("/").filter((path) => path);
 
-    const { product } = props;
+  return (
+    <Breadcrumb style={{ margin: "2rem 0 0 0" }}>
+      <BreadcrumbItem onClick={() => router.push("/")}>
+        {homeElement}
+      </BreadcrumbItem>
+      {pathNames.map((link, index) => {
+        let href = `/${pathNames.slice(0, index + 1).join("/")}`;
+        let itemClasses = paths === href ? `${activeClasses}` : listClasses;
 
-    return (
-    <>
-        <nav aria-label="breadcrumb">
-            <ol className="breadcrumb">
-                <li className="breadcrumb-item">
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li className="breadcrumb-item">
-                    <a href="#">Product</a>
-                </li>
-                <li className="breadcrumb-item active" aria-current="page">
-                    { product.name }
-                </li>
-            </ol>
-        </nav>
-    </>
-    )
+        let itemLink = capitalizeLinks
+          ? link[0].toUpperCase() + link.slice(1, link.length)
+          : link;
+
+        return (
+          <BreadcrumbItem
+            key={href}
+            className={itemClasses}
+            onClick={() =>
+              href !== "/product" && router.push(`/product/${slug}`)
+            }
+          >
+            {itemLink}
+          </BreadcrumbItem>
+        );
+      })}
+    </Breadcrumb>
+  );
 }
