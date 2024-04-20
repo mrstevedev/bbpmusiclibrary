46ad27b6be73360ca603b8a5fa4da3b3d48e0b79
Steven <stevendotpulido@gmail.com>
2024-04-20 00:29:18 -0700

    Add ProductSingle.tsx component

--- components/product/ProductSingle.tsx
+++ components/product/ProductSingle.tsx
@@ -1,77 +1,80 @@
-import styles from "../../styles/Product.module.scss";
 import Image from "next/image";
+import Link from "next/link";
+
+import styles from "@/styles/Product.module.scss";
+
 import AddCartButton from "./AddCartButton";
-import { createLink } from "../../util";
+import { Col, Row } from "react-bootstrap";
+import { TProductSingle } from "@/types/types";
 
-interface IProduct {
-    addToCart: boolean
-    handleAddToCart: () => boolean
-    handleShowImageGallery: () => boolean
-    product: {
-        name: string,
-        description: string
-        price: string
-        image: {
-            mediaItemUrl: string
-        }
-        productCategories: {
-            nodes: {
+export default function ProductSingle({
+  product,
+  handleShowImageGallery,
+  handleAddToCart,
+}: TProductSingle) {
+  const { name, description, salePrice, regularPrice } = product;
+  const { mediaItemUrl } = product.image;
 
-            }
-        }
-        salePrice: string
-        regularPrice: string
-    }
-}
+  const categories = product.productCategories.nodes;
 
-export default function ProductSingle(props: IProduct) {
-
-  const { product } = props;
-    const { name, description, price } = props.product;
-    const { mediaItemUrl } = props.product.image;
-
-    const categories = Object.values(props.product.productCategories.nodes);
-
-    return (
-        <>
-            <div className={styles.productTop}>
-                <a href="#" onClick={props.handleShowImageGallery}>
-                <div className={`product-img ${styles.product__Img}`}>
-                    <Image
-                    loading="eager"
-                    src={mediaItemUrl}
-                    width="490"
-                    height="490"
-                    alt={`Product Image - ${ name }`} 
-                    />
-                </div>
-                </a>
-                    <div className={styles.productDescription}>
-                    <h3 className={styles.productDescriptionTxt}>{name}</h3>
-                    <h4>{ product.salePrice ? (
-                    <>
-                      <span className={styles.Product__RegularPrice}>{ product.regularPrice }</span>
-                      <span className={styles.Product__SaleTxt}> { product.salePrice }</span>
-                    </>
-                    ) : product.regularPrice }</h4>
-                    <p>{description}</p>
-                    <h4 className={styles.productCategoriesTxt}>
-                    Categories:{" "}
-                    <span
-                      className="category__wrapper"
-                      style={{ color: "#1a1a1a" }}
-                      dangerouslySetInnerHTML={{
-                      __html: createLink(categories, "a"),
-                      }}
-                    ></span>
-                    </h4>
-                    <AddCartButton product={product}
-                      addToCart={props.addToCart}
-                      handleAddToCart={props.handleAddToCart}
-                      productPage
-                    />
-                    </div>
-                    </div>
-        </>
-    )
+  return (
+    <Row className={styles.productTop}>
+      <Col>
+        <a href="#" onClick={handleShowImageGallery}>
+          <div className={`product-img ${styles.product__Img}`}>
+            <Image
+              loading="eager"
+              src={mediaItemUrl}
+              width="490"
+              height="490"
+              alt={`Product Image - ${name}`}
+            />
+          </div>
+        </a>
+      </Col>
+      <Col>
+        <div className={styles.productDescription}>
+          <h3 className={styles.productDescriptionTxt}>{name}</h3>
+          <h4>
+            {salePrice ? (
+              <>
+                <span className={styles.Product__RegularPrice}>
+                  {regularPrice}
+                </span>
+                <span className={styles.Product__SaleTxt}> {salePrice}</span>
+              </>
+            ) : (
+              regularPrice
+            )}
+          </h4>
+          <p>{description}</p>
+          <h4 className={styles.productCategoriesTxt}>
+            Categories:{" "}
+            {categories.map((obj) => (
+              <Link
+                key={obj.name}
+                href={{
+                  pathname: `/category/${obj["name"]
+                    .toLowerCase()
+                    .replace(" ", "-")}`,
+                }}
+                passHref
+              >
+                <span
+                  style={{
+                    margin: "0 0.1rem",
+                    cursor: "pointer",
+                    color: "#446dd2",
+                  }}
+                >
+                  <a className="link-blue">{obj["name"]}</a>
+                </span>
+              </Link>
+            ))}
+          </h4>
+          <AddCartButton product={product} handleAddToCart={handleAddToCart} />
+        </div>
+      </Col>
+    </Row>
+  );
 }
