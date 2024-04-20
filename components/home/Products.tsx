4d8c0a7397137b557f6bfe1f60321c52e77d3c47
Steven <stevendotpulido@gmail.com>
2024-04-19 17:19:04 -0700

    Add Products.tsx component

--- components/home/Products.tsx
+++ components/home/Products.tsx
@@ -1,38 +1,51 @@
-import styles from '../../styles/Products.module.scss'
-import ProductItem from './ProductItem'
+import styles from "@/styles/Products.module.scss";
+import ProductItem from "../Home/ProductItem";
+import { Col, Container, Row } from "react-bootstrap";
+import Image from "next/image";
+
+import amazonAd from "../../public/images/Amazon-Display-Ad-Arlo.png";
 
 interface IProduct {
   node: {
-    id: string
+    id: string;
     image: {
-      mediaItemUrl: string
-    }
-    name: string
-    price: string
-    slug: string
-    salePrice: string
-    regularPrice: string
-  }
+      mediaItemUrl: string;
+    };
+    name: string;
+    price: string;
+    slug: string;
+    salePrice: string;
+    regularPrice: string;
+  };
 }
 
 interface IProducts {
-    products: IProduct[]
+  products: IProduct[];
 }
 
-export default function Products({ products } : IProducts) {
-
-    return (
-        <>
-          <div className={`row ${ styles.products }`}>
-            <h1>
-            Latest Sample Pack Releases 
-            <span> Showing { products.length }</span></h1>
-            {products.map((product) => {
-                return (
-                    <ProductItem key={product.node.id} product={product} />
-                  )
-                })}
-          </div>
-        </>
-    )
+export default function Products({ products }: IProducts) {
+  return (
+    <Container fluid className={`${styles.Products}`}>
+      <h1>
+        Latest Releases
+        <span> Showing {products?.length}</span>
+      </h1>
+      <Row>
+        <Col lg="10">
+          <Row>
+            {products?.map((product) => {
+              return (
+                <Col key={product.node.name} xs="12" sm="5" lg="4" xl="3">
+                  <ProductItem key={product.node.id} product={product} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Col>
+        <Col className={styles.Products__ad}>
+          <Image src={amazonAd} alt="amazonAd" />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
