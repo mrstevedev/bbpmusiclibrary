2b79055169791413bd31d1d20175ff206bf0fa64
Steven <stevendotpulido@gmail.com>
2024-04-20 00:34:11 -0700

    Add cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,73 +1,50 @@
-import styles from '../styles/Cart.module.scss'
-import { ChangeEvent, useContext, useEffect, useState } from 'react'
-import { AppContext } from '../components/context/AppContext'
-import Cart from '../components/cart/Cart'
-import Script from 'next/script'
-import CoverImage from '../components/coverimages/CoverImage'
-import { removeItemFromCart } from '../util'
+import { useContext } from "react";
+import styles from "@/styles/Cart.module.scss";
+import Script from "next/script";
+import Cart from "@/components/Cart/Cart";
+import CoverImage from "@/components/CoverImage/CoverImage";
+import { CartContext, TCartContext } from "@/context/CartContext";
+import { removeItemFromCart } from "@/util/index";
+import { Container } from "react-bootstrap";
 
 const CartPage = () => {
-    const [cart, setCart] = useContext<any>( AppContext )
-    
-    const products =
-    ( cart && Object.keys( cart ).length ) ? cart.products : ""
+  const { cart, setCart } = useContext<TCartContext>(CartContext);
 
-    const price = 
-    ( null != cart && Object.keys( cart ).length ) ? cart.products[0].price : ""
+  const products = cart && Object.keys(cart).length ? cart.products : [];
 
-    const productsCount =
-    ( null != cart && Object.keys( cart ).length ) ? cart.totalProductsCount : "" 
-    
-    const [quantity, setQuantity] = useState(productsCount)
+  const totalProductsPrice =
+    null != cart && Object.keys(cart).length ? cart.totalProductsPrice : 0;
 
-    const [totalStatePrice, setTotalStatePrice] = useState(price)
+  const handleRemoveItem = (event: any, id: number) => {
+    removeItemFromCart(id);
+    setCart({});
+  };
 
-    useEffect(() => {
-        setQuantity(productsCount)
-    }, [setQuantity])
+  return (
+    <>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `document.body.classList.remove('Checkout__page');
+                document.body.classList.add('Cart__page')`,
+        }}
+      />
+      <Container fluid>
+        <CoverImage />
+        <div className="container">
+          <div className={styles.cart}>
+            <div className={styles.cart__header}>
+              <h3 className={styles.cart__headerText}>Shopping Cart</h3>
+            </div>
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setQuantity(Number(event.target.value))
-
-        console.log(quantity)
-
-        // setTotalStatePrice(totalStatePrice * quantity)
-
-    }
-
-    const handleRemoveItem = (event: any, id: number) => {
-        removeItemFromCart(id)
-        setCart(null)
-    }
-
-    return (
-        <>
-        <Script
-            dangerouslySetInnerHTML={{
-            __html: `document.body.classList.remove('Checkout__page');
-                document.body.classList.add('Cart__page')`
-            }}
-        />
-            <main>
-            <CoverImage />
-                <div className="container">
-                    <div className={ styles.cart }>
-                        <div className={ styles.cart__header } >
-                            <h3 className={ styles.cart__headerText }>Shopping Cart</h3>
-                        </div>
-
-                        <Cart 
-                            products={products}
-                            quantity={quantity}
-                            handleChange={handleChange}
-                            totalStatePrice={totalStatePrice}
-                            handleRemoveItem={handleRemoveItem}
-                        />
-                        
-                    </div>
-                </div>
-            </main>
-        </>
-    )
-} 
-export default CartPage
+            <Cart
+              products={products}
+              handleRemoveItem={handleRemoveItem}
+              totalProductsPrice={totalProductsPrice}
+            />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+export default CartPage;
