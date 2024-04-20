7ac5b9de9bfd4cce213c6587fd38453bac213aa7
Steven <stevendotpulido@gmail.com>
2024-04-20 00:32:07 -0700

    Add Soundcloud.tsx component

--- components/soundcloud/Soundcloud.tsx
+++ components/soundcloud/Soundcloud.tsx
@@ -1,56 +1,73 @@
-import styles from '../../styles/Soundcloud.module.scss'
+import styles from "@/styles/Soundcloud.module.scss";
 
-interface IDownloads {
-    file: string
-}
-interface IRelated {
-    node: {
-        id: string
-        image: {
-            mediaItemUrl: string
-        }
-        name: string
-        slug: string
-    }
-}
+type Categories = {
+  name: string;
+};
 
-interface ICats {
-   id: string
-   name: string   
-}
+type Related = {
+  node: {
+    id: string;
+    image: {
+      mediaItemUrl: string;
+    };
+    name: string;
+    slug: string;
+  };
+};
 
-interface IProduct {
-    product: {
-        databaseId: number
-        description: string
-        downloadable: boolean
-        downloads: IDownloads[]
-        image: {
-            mediaItemUrl: string
-            id: string
-        }
-        name: string
-        price: string
-        productCategories: {
-            nodes: ICats[]
-        }
-        shortDescription: string
-        related: {
-            edges: IRelated[]
-        }
-        sku: string
-    }
-}
+type Product = {
+  product: {
+    databaseId: number;
+    description: string;
+    downloadable: true;
+    image: {
+      mediaItemUrl: string;
+      id: string;
+    };
+    name: string;
+    price: string;
+    productCategories: {
+      nodes: Categories[];
+    };
+    regularPrice: string;
+    related: { edges: Related[] };
+    salePrice: string;
+    shortDescription: string;
+    sku: string;
+    slug: string;
+  };
+};
 
-export default function Soundcloud(props: IProduct) {
-    const { sku, name, shortDescription } = props.product
-    return (
-        <>
-         <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src={ `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${ sku }&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true` }></iframe>
-            <div>
-                <a href="https://soundcloud.com/marioluciano425" title="Polyphonic Music Library" target="_blank" rel="noreferrer" className={ styles.Soundcloud__name }>Polyphonic Music Library</a> · 
-                <a href={ shortDescription } title="1.Soul Expressions (Kit Preview)" target="_blank" rel="noreferrer" className={ styles.Soundcloud__shortDescription }> { name }</a>
-            </div>
-        </>
-    )
+export default function Soundcloud({ product }: Product) {
+  return (
+    <>
+      <iframe
+        width="100%"
+        height="300"
+        src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${product.sku}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
+      ></iframe>
+      <div>
+        <a
+          href=""
+          title="Polyphonic Music Library"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.Soundcloud__name}
+        >
+          Polyphonic Music Library
+        </a>{" "}
+        ·
+        <a
+          href={product.shortDescription}
+          title="1.Soul Expressions (Kit Preview)"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.Soundcloud__shortDescription}
+        >
+          {" "}
+          {product.name}
+        </a>
+      </div>
+    </>
+  );
 }
