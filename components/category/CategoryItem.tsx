464d87fc94fe093411d337245169010b60d81275
Steven <stevendotpulido@gmail.com>
2024-04-19 17:00:19 -0700

    Add CategoryItem.tsx component

--- components/category/CategoryItem.tsx
+++ components/category/CategoryItem.tsx
@@ -1,50 +1,61 @@
-
-import styles from '../../styles/Category.module.scss'
-import Link from "next/link"
-import Image from "next/image"
-import { trimString } from '../../util/index'
+import styles from "../../styles/Category.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { trimString } from "../../util/index";
+import { Button } from "react-bootstrap";
 
 interface IProduct {
-    name: string
-    slug: string
-    image: {
-        mediaItemUrl: string
-    }
-    description: string 
-    price: number
+  description: string;
+  image: { mediaItemUrl: string };
+  name: string;
+  price: string;
+  slug: string;
 }
 
 interface IProducts {
-    products: IProduct[]
+  products: IProduct[];
 }
 
-export default function CategoryItem({ products } : IProducts) {
-    return (
-        <>
-            {products.map((product : IProduct) => (
-                <div className={styles.category__row} key={product.name}>
-                    <div className="product-img">
-                        <Link href={{
-                            pathname: `/product/${ product.slug }`
-                        }}>
-                            <a>
-                                <Image
-                                    src={product.image.mediaItemUrl}
-                                    alt="Category Image"
-                                    width="350"
-                                    height="350"
-                                    className={styles.category__row__img}
-                                />
-                            </a>
-                        </Link>
-                    </div>
-                <div className={ styles.category__results_row_description }>
-                    {product.name}
-                    <p>{ product.description !== null ? trimString(product.description, 250) : '' }</p>
-                    <p className={ styles.category__price }>Price: { product.price }</p>
-                </div>
-            </div>
-          ))}
-        </>
-    )
+export default function CategoryItem({ products }: IProducts) {
+  return (
+    <>
+      {products.map((product: IProduct) => (
+        <div className={styles.category__row} key={product.name}>
+          <div className="product-img">
+            <Link
+              href={{
+                pathname: `/product/${product.slug}`,
+              }}
+            >
+              <a>
+                <Image
+                  src={product.image.mediaItemUrl}
+                  alt="Category Image"
+                  width="350"
+                  height="350"
+                  className={styles.category__row__img}
+                />
+              </a>
+            </Link>
+          </div>
+          <div className={styles.category__results_row_description}>
+            <strong>{product.name}</strong>
+            <p>
+              {product.description !== null
+                ? trimString(product.description, 250)
+                : ""}
+            </p>
+            <p className={styles.category__price}>Price: {product.price}</p>
+            <Link
+              href={{
+                pathname: `/product/${product.slug}`,
+              }}
+            >
+              <Button>Go to product</Button>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
