98871effe2535dd35f80ca54923d42d1906855f4
Steven <stevendotpulido@gmail.com>
2024-04-19 17:16:12 -0700

    Add ProductItem.tsx component

--- components/home/ProductItem.tsx
+++ components/home/ProductItem.tsx
@@ -1,59 +1,60 @@
-import { Fragment } from 'react'
-import Image from "next/image"
-import Link from "next/link"
-import styles from "../../styles/Home.module.scss"
+import { Fragment } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/Home.module.scss";
 
 interface IProduct {
   product: {
     node: {
-      id: string
-      name: string
-      price: string
-      slug: string
+      id: string;
+      name: string;
+      price: string;
+      slug: string;
       image: {
-        mediaItemUrl: string
-      }
-      salePrice: string
-      regularPrice: string
-    }
-  }
+        mediaItemUrl: string;
+      };
+      salePrice: string;
+      regularPrice: string;
+    };
+  };
 }
 
-export default function ProductItem({ product } : IProduct) {
+export default function ProductItem({
+  product: {
+    node: { id, name, slug, image: img, salePrice, regularPrice },
+  },
+}: IProduct) {
   return (
-    <Fragment key={product.node.id}>
-      <div className="col-md-3 d-flex flex-column justify-content-center">
-        <Link href={`/product/${product.node.slug}`}>
-          <a>
-            <div className={`${styles.Home__image} product-img`}>
-              <Image
-                key={product.node.id}
-                src={
-                  product.node.image !== null
-                    ? product.node.image.mediaItemUrl
-                    : "http://localhost:10028/wp-content/uploads/2021/10/websiteplanet-dummy-640X640.webp"
-                }
-                width="400"
-                height="400"
-                loading="eager"
-                alt={`Bonita Basics Productions Music Library, ${ product.node.name }`}
-              />
-            </div>
-          </a>
-        </Link>
-        <div className={styles.productBtm}>
-          <Link href={`/product/${product.node.slug}`}>
-            <a className="link">{product.node.name}</a>
-          </Link>
-          <p>{ product.node.salePrice ? (
-            <>
-              <span className={styles.product__regularPrice}>
-                {product.node.regularPrice} </span> { product.node.salePrice } 
-                <span className={styles.product__salePrice}>
-              </span>
-            </>
-          ) : product.node.regularPrice }</p>
+    <Fragment key={id}>
+      <Link href={`/product/${slug}`} passHref>
+        <div className={`${styles.Home__image} product-img`}>
+          <Image
+            key={id}
+            src={img.mediaItemUrl}
+            width="400"
+            height="400"
+            loading="eager"
+            alt={`Bonita Basics Productions Music Library, ${name}`}
+          />
         </div>
+      </Link>
+      <div className={styles.productBtm}>
+        <Link href={`/product/${slug}`}>
+          <a className="link">{name}</a>
+        </Link>
+        <p>
+          {salePrice ? (
+            <Fragment>
+              <span className={styles.product__regularPrice}>
+                {regularPrice}{" "}
+              </span>{" "}
+              {salePrice}
+              <span className={styles.product__salePrice}></span>
+            </Fragment>
+          ) : (
+            regularPrice
+          )}
+        </p>
       </div>
     </Fragment>
   );
