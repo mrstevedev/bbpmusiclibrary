6b1c645bcfe4f5efaad96ec7d4e52ebdd77a9c81
Steven <stevendotpulido@gmail.com>
2024-04-23 13:40:04 -0700

    Add <Head> tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+          />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </Head>
         <body>
           <Main />
           <NextScript />
