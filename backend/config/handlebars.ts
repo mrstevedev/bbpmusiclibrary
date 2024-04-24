bd77f2a8c30feaf078af7f202b37c97611ad3b4d
Steven <stevendotpulido@gmail.com>
2024-04-23 17:28:21 -0700

    Update option to named export

--- backend/config/handlebars.ts
+++ backend/config/handlebars.ts
@@ -1,13 +1,11 @@
 import path from "path";
 
-const handlebarOptions = {
+export const handlebarOptions = {
   viewEngine: {
     extname: ".hbs",
     layoutsDir: "backend/templates/emails",
-    defaultLayout: false,
     partialsDir: "backend/templates/emails",
   },
   viewPath: path.resolve("backend/templates/emails"),
   extName: ".hbs",
 };
-module.exports = handlebarOptions;
