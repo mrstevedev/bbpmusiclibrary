f26c9cb881321b191e1f8603e66619fa0d385314
Steven <stevendotpulido@gmail.com>
2024-04-29 23:25:20 -0700

    Add util

---
+++ util/constants.ts
@@ -0,0 +1,20 @@
+export const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const COUPON_USED = "Coupon has already been used";
+export const COUPON_INVALID = "The coupon provided is invalid";
+export const COUPON_NOT_PROVIDED = "No coupon was provided";
+export const EMAIL_NOT_PROVIDED = "No email was provided";
+export const PASSWORD_NOT_MATCH = "Passwords do not match";
