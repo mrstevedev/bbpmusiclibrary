8483fdf83d6675c3e3ead0fe5c25987f4f38a19b
Steven <stevendotpulido@gmail.com>
2024-07-01 23:27:29 -0700

    Add useValidateToken hook

---
+++ src/hooks/useValidateToken.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export const useValidateToken = () => {
+  const router = useRouter();
+  const [isTokenExpired, setIsTokenExpired] = useState(false);
+
+  const user =
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("bbp_user") as string)
+      : false;
+
+  useEffect(() => {
+    if (user) {
+      const decodedJwt = JSON.parse(atob(user?.token?.split(".")[1]));
+      console.log("jwt expires:", new Date(decodedJwt.exp * 1000));
+      if (decodedJwt.exp * 1000 < Date.now()) {
+        setIsTokenExpired(true);
+        localStorage.removeItem("bbp_user");
+        localStorage.removeItem("bbp_product");
+        toast.warn("Your session has expired.");
+      } else {
+        setIsTokenExpired(false);
+      }
+    }
+  }, [isTokenExpired, router, user]);
+
+  return isTokenExpired;
+};
