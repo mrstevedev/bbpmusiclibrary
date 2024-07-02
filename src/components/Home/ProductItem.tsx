a9a17a371cb66172fe3122d68b8954dd34e637fa
Steven <stevendotpulido@gmail.com>
2024-07-01 23:35:25 -0700

    Add ProductItem component

--- src/components/Home/ProductItem.tsx
+++ src/components/Home/ProductItem.tsx
@@ -1,44 +1,51 @@
 import { Fragment } from "react";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "react-bootstrap/Image";
 import styles from "@/styles/Home.module.scss";
-import { TProduct } from "src/types/types";
+import { HomeProductItem } from "@/types/types";
+import { Col } from "react-bootstrap";
 
-export default function ProductItem({ product }: TProduct) {
-  const { id, name, image, slug, salePrice, regularPrice } = product;
+export default function ProductItem({ product, id }: HomeProductItem) {
+  const { databaseId, name, image, slug, salePrice, regularPrice } = product;
   return (
-    <Fragment key={id}>
+    <Col
+      data-testid={`product-item-${id}`}
+      key={databaseId}
+      xs="12"
+      sm="5"
+      lg="4"
+      xl="3"
+    >
       <Link href={`/product/${slug}`} passHref>
-        <div className={`${styles.Home__image} product-img`}>
+        <div className={`${styles.BBP_Home__Image} product-img`}>
           <Image
-            key={id}
             src={image.mediaItemUrl}
             width="400"
             height="400"
-            layout="responsive"
             loading="eager"
-            alt={`Bonita Basics Productions Music Library, ${name}`}
+            className="w-100 h-100"
+            alt={`Bonita Basics Productions Music Library ${name}`}
           />
         </div>
       </Link>
-      <div className={styles.productBtm}>
+      <div className={styles.BBP_Product__Bottom}>
         <Link href={`/product/${slug}`} className="link">
           {name}
         </Link>
         <p>
           {salePrice ? (
             <Fragment>
-              <span className={styles.product__regularPrice}>
+              <span className={styles.BBP_Product__RegularPrice}>
                 {regularPrice}{" "}
               </span>{" "}
               {salePrice}
-              <span className={styles.product__salePrice}></span>
+              <span className={styles.BBP_Product__SalePrice}></span>
             </Fragment>
           ) : (
             regularPrice
           )}
         </p>
       </div>
-    </Fragment>
+    </Col>
   );
 }
