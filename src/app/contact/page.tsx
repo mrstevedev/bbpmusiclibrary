da9d6e1270e8160c77bef7f4c902e8218439af5d
Steven <stevendotpulido@gmail.com>
2024-07-02 10:13:41 -0700

    Add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,39 @@
+import { Container } from "react-bootstrap";
+import { GET_CONTACT_PAGE } from "@/query/index";
+import Banner from "@/components/Banner/Banner";
+import ContactForm from "@/components/Forms/Contact/ContactForm";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "BBP Music Library | Bonita Basics Productions Music Library | Contact",
+  description:
+    "About BBPMusicLibrary, Sample Curator, Digital download Sample Packs for Hip Hop, Boom Bap music production",
+};
+
+export default async function Contact() {
+  const { data } = await getData();
+  const imgUrl = data.page.featuredImage.node.mediaItemUrl;
+
+  return (
+    <Container as="main" fluid>
+      <Banner contact mediaItemUrl={imgUrl} />
+      <ContactForm />
+    </Container>
+  );
+}
+
+async function getData() {
+  const res = await fetch(process.env.GRAPHQL_URL as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: GET_CONTACT_PAGE,
+      variables: { id: 18649 },
+    }),
+  });
+  return res.json();
+}
