3ac9a2bd5ce34510f67ef240e8c2805eab62e5f8
Steven <stevendotpulido@gmail.com>
2024-07-02 10:31:12 -0700

    Add contact route handler

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,30 @@
+import { resend } from "src/app/config/resend";
+import { render } from "@react-email/render";
+import ContactTemplate from "emails/contact";
+
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const { firstName, lastName, message, email } = body;
+  const issue = body.issue.label;
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "no-reply@bbpmusiclibrary.com",
+      to: "support@bbpmusiclibrary.com",
+      subject: issue,
+      react: ContactTemplate({ firstName, lastName, email, issue, message }),
+    });
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({
+        error: {
+          message: error.message,
+        },
+      });
+    }
+  }
+}
