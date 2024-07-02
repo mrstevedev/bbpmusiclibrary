8c73e703a301c2d9f4aaf743504f85b1487e6d96
Steven <stevendotpulido@gmail.com>
2024-07-01 22:59:34 -0700

    Add CheckoutForm component

--- src/components/Forms/Checkout/CheckoutForm.tsx
+++ src/components/Forms/Checkout/CheckoutForm.tsx
@@ -1,21 +1,165 @@
 import Select from "react-select";
-import { useState, useMemo } from "react";
 import { useStates } from "react-us-states";
 import styles from "@/styles/Checkout.module.scss";
 import countryList from "react-select-country-list";
-import SubmitPaymentButton from "src/components/Buttons/SubmitPaymentsButton";
-import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useMemo, useCallback, useContext } from "react";
 import { Form, FormControl, InputGroup, Row } from "react-bootstrap";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-export default function CheckoutForm({
-  values,
-  handleSubmit,
-  handleChange,
-  setFieldValue,
-}) {
+import { useFormik } from "formik";
+import AccordionPay from "@/components/Checkout/Accordion/AccordionPay";
+import { CREDIT_CARD, METHOD_PAYPAL, METHOD_STRIPE } from "@/constants/index";
+import PayPalButtonCheckout from "@/components/Forms/Checkout/Button/PayPalButtonCheckout";
+import SubmitPaymentStripeButton from "@/components/Forms/Checkout/Button/SubmitPaymentStripeButton";
+
+import { CouponContext, TCouponContext } from "@/context/CouponContext";
+
+import {
+  useStripe,
+  useElements,
+  CardNumberElement,
+} from "@stripe/react-stripe-js";
+
+import { object, string } from "yup";
+import { toast } from "react-toastify";
+
+const CheckoutSchema = object().shape({
+  email: string().email("Must be a valid email").required(),
+  country: string().required(),
+  firstName: string().required(),
+  lastName: string().required(),
+  address: string().required(),
+  apartment: string().required(),
+  city: string().required(),
+  zip: string().required(),
+  state: string().required(),
+});
+
+export default function CheckoutForm({ purchaseUnits }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const [countryValue] = useState({});
   const [usStates] = useState({});
   const countryOptions = useMemo(() => countryList().getData(), []);
+  const [paymentType, setPaymentType] = useState(CREDIT_CARD);
+  const [processing, setProcessing] = useState(false);
+
+  const { coupon } = useContext<TCouponContext>(CouponContext);
+
+  const createQueryString = useCallback(
+    (
+      success: string,
+      successValue: string,
+      email: string,
+      emailValue: string,
+      orderID: string,
+      orderIDValue: string
+    ) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(success, successValue);
+      params.set(email, emailValue);
+      params.set(orderID, orderIDValue);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const { values, handleSubmit, handleChange, setFieldValue, touched, errors } =
+    useFormik({
+      initialValues: {
+        email: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+        apartment: "",
+        city: "",
+        zip: "",
+        state: "",
+        country: "",
+        card: false,
+        expiration: false,
+        securityCode: false,
+        nameOnCard: "",
+      },
+      validationSchema: CheckoutSchema,
+      onSubmit: async (values) => {
+        setProcessing(true);
+
+        const cardElement = elements?.getElement(CardNumberElement);
+
+        if (!stripe || !cardElement) return null;
+
+        const name = values.firstName + " " + values.lastName;
+        const email = values.email;
+        const username = values.email;
+        const address = values.address;
+        const city = values.city;
+        const state = values.state;
+        const zip = values.zip;
+        const country_code = values.country;
+
+        const payload = JSON.stringify({
+          name,
+          email,
+          username,
+          address,
+          city,
+          state,
+          postal_code: zip,
+          country_code,
+          payment_method: METHOD_STRIPE,
+          purchaseUnits,
+          coupon: coupon?.isApplied ? coupon.code : null,
+        });
+
+        try {
+          const response = await fetch("/api/create", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: payload,
+          });
+
+          const data = await response.json();
+
+          if (!response.ok) {
+            toast.error(data.message);
+            return;
+          }
+
+          const clientSecret = data.clientSecret;
+
+          const res = await stripe?.confirmCardPayment(clientSecret, {
+            payment_method: { card: cardElement },
+          });
+
+          if (res.paymentIntent) {
+            setProcessing(false);
+
+            router.push(
+              "/confirm" +
+                "?" +
+                createQueryString(
+                  "success",
+                  "true",
+                  "email",
+                  values.email,
+                  "transactionID",
+                  res.paymentIntent.id
+                )
+            );
+          }
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            toast.error(error.message);
+          }
+        }
+      },
+    });
 
   const states = useStates().map((state) => state);
   const newStates = states.map(({ name, abbreviation }) => ({
@@ -35,40 +179,63 @@ export default function CheckoutForm({
   );
 
   return (
-    <Form id="my-form" onSubmit={handleSubmit}>
-      <h3 className={styles.Checkout_heading}>Contact</h3>
+    <Form style={{ margin: "0 0 4rem 0" }} onSubmit={handleSubmit}>
+      <h5 className={styles.Checkout_heading}>Contact</h5>
       <InputGroup className="mb-2">
         <FormControl
           type="email"
           id="email"
           name="email"
-          className={`form-control`}
+          className={`form-control ${
+            touched.email && errors.email ? "error" : null
+          }`}
           aria-describedby="emailHelp"
           placeholder="Email"
           onChange={handleChange}
           value={values?.email}
+          style={{ fontSize: "0.9rem", height: "47px" }}
           autoFocus
-          required
         />
       </InputGroup>
 
       <InputGroup>
-        <Form.Check onChange={handleChange} type="checkbox" />
-        <label className="form-check-label" htmlFor="exampleCheck1">
+        <Form.Check onChange={handleChange} type="checkbox" id="form-check" />
+        <label
+          style={{ display: "flex", alignItems: "center" }}
+          className="form-check-label"
+          htmlFor="form-check"
+        >
           Email me with news and offers
         </label>
       </InputGroup>
 
-      <h3 className={styles.Checkout_heading}>Billing Address</h3>
+      <h5 className={styles.Checkout_heading}>Payment</h5>
+      <p style={{ fontSize: "0.9rem", fontWeight: 100 }}>
+        All transactions are secure and encrypted.
+      </p>
+
+      <AccordionPay
+        errors={errors}
+        touched={touched}
+        handleChange={handleChange}
+        purchaseUnits={purchaseUnits}
+        setPaymentType={setPaymentType}
+        setFieldValue={setFieldValue}
+      />
+
+      <h5 className={styles.Checkout_heading} style={{ fontSize: "1rem" }}>
+        Billing Address
+      </h5>
+
       <InputGroup className="mb-3">
         <Select
           id="country"
           name="country"
-          defaultValue={{ label: "Country/Region" }}
+          defaultValue={{ label: "Country/Region", value: "" }}
           options={countryOptions}
           instanceId="long-value-select"
           val={countryValue}
-          onChange={(option) => setFieldValue("country", option)}
+          onChange={(option) => setFieldValue("country", option?.value)}
           theme={(theme) => ({
             ...theme,
             colors: {
@@ -82,9 +249,15 @@ export default function CheckoutForm({
               ...baseStyles,
               width: "100%",
             }),
+            option: (baseStyles) => ({
+              ...baseStyles,
+            }),
             control: (baseStyles) => ({
               ...baseStyles,
-              borderColor: "#dee2e6",
+              height: "47px",
+              fontSize: "0.9rem",
+              borderColor:
+                touched.country && errors.country ? "red" : "#dee2e6",
             }),
           }}
         />
@@ -95,21 +268,25 @@ export default function CheckoutForm({
           onChange={handleChange}
           type="text"
           name="firstName"
-          className={`form-control`}
+          className={`form-control  ${
+            touched.firstName && errors.firstName ? "error" : null
+          }`}
           id="firstName"
           aria-describedby="firstName"
           placeholder="First Name"
-          required
+          style={{ fontSize: "0.9rem", height: "47px" }}
         />
         <FormControl
           onChange={handleChange}
           type="text"
           name="lastName"
-          className={`form-control`}
+          className={`form-control  ${
+            touched.lastName && errors.lastName ? "error" : null
+          }`}
           id="lastName"
           aria-describedby="lastName"
           placeholder="Last Name"
-          required
+          style={{ fontSize: "0.9rem", height: "47px" }}
         />
       </InputGroup>
 
@@ -117,13 +294,16 @@ export default function CheckoutForm({
         <GooglePlacesAutocomplete
           apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACES_API}
           selectProps={{
-            onChange: (option) => setFieldValue("select", option),
+            id: "address",
+            name: "address",
+            onChange: (option) => setFieldValue("address", option?.label),
             placeholder: "Address",
             styles: {
               control: (baseStyles) => ({
                 ...baseStyles,
                 boxShadow: "none",
-                borderColor: "#dee2e6",
+                borderColor:
+                  touched.address && errors.address ? "red" : "#dee2e6",
                 "&:hover": {
                   borderColor: "hsl(0, 0%, 70%)",
                 },
@@ -136,6 +316,8 @@ export default function CheckoutForm({
                 ...provided,
                 fontWeight: 100,
                 color: "black",
+                fontSize: "0.9rem",
+                height: "36px",
               }),
               placeholder: (provided) => ({
                 ...provided,
@@ -154,15 +336,15 @@ export default function CheckoutForm({
         />
 
         {/* <FormControl
-      onChange={handleChange}
-      type="search"
-      name="address"
-      className={`form-control`}
-      id="address"
-      aria-describedby="address"
-      placeholder="Address"
-      required
-    /> */}
+          onChange={handleChange}
+          type="search"
+          name="address"
+          className={`form-control`}
+          id="address"
+          aria-describedby="address"
+          placeholder="Address"
+          required
+        /> */}
       </InputGroup>
 
       <InputGroup className="mb-3">
@@ -170,11 +352,13 @@ export default function CheckoutForm({
           onChange={handleChange}
           type="text"
           name="apartment"
-          className={`form-control`}
+          className={`form-control  ${
+            touched.apartment && errors.apartment ? "error" : null
+          }`}
           id="apartment"
           aria-describedby="emailHelp"
           placeholder="Apartment, Suite (Optional)"
-          required
+          style={{ fontSize: "0.9rem", height: "47px" }}
         />
       </InputGroup>
 
@@ -182,22 +366,26 @@ export default function CheckoutForm({
         <FormControl
           onChange={handleChange}
           type="text"
-          name="city"
-          className={`form-control`}
           id="city"
+          name="city"
+          className={`form-control  ${
+            touched.city && errors.city ? "error" : null
+          }`}
           aria-describedby="emailHelp"
           placeholder="City"
-          required
+          style={{ fontSize: "0.9rem", height: "47px" }}
         />
         <FormControl
           type="text"
           onChange={handleChange}
           name="zip"
-          className={`form-control`}
+          className={`form-control  ${
+            touched.zip && errors.zip ? "error" : null
+          }`}
           id="zip"
           aria-describedby="zip"
           placeholder="Zip Code"
-          required
+          style={{ fontSize: "0.9rem", height: "47px" }}
         />
       </InputGroup>
 
@@ -208,7 +396,7 @@ export default function CheckoutForm({
           defaultValue={{ label: "Select State" }}
           options={updatedStates}
           val={usStates}
-          onChange={(option) => setFieldValue("state", option)}
+          onChange={(option) => setFieldValue("state", option?.label)}
           theme={(theme) => ({
             ...theme,
             colors: {
@@ -224,7 +412,9 @@ export default function CheckoutForm({
             }),
             control: (baseStyles) => ({
               ...baseStyles,
-              borderColor: "#dee2e6",
+              height: "47px",
+              fontSize: "0.9rem",
+              borderColor: touched.state && errors.state ? "red" : "#dee2e6",
             }),
           }}
         />
@@ -232,7 +422,15 @@ export default function CheckoutForm({
 
       <Row className="row mb-3">
         <div className="d-grid col-sm-12 col-md-12">
-          <SubmitPaymentButton />
+          {paymentType === CREDIT_CARD ? (
+            <SubmitPaymentStripeButton processing={processing} />
+          ) : paymentType === METHOD_PAYPAL ? (
+            <PayPalButtonCheckout
+              values={values}
+              purchaseUnits={purchaseUnits}
+              handleSubmit={handleSubmit}
+            />
+          ) : null}
         </div>
       </Row>
     </Form>
