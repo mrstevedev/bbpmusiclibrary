6b842766a9d1e07ae8d561fc55000d94f3713513
Steven <stevendotpulido@gmail.com>
2024-07-02 00:49:05 -0700

    Add CartItem component

--- src/components/SidebarCart/CartItem.tsx
+++ src/components/SidebarCart/CartItem.tsx
@@ -1,41 +1,65 @@
+"use client";
 import Link from "next/link";
-import Image from "next/image";
-
-import styles from "@/styles/SidebarCart.module.scss";
+import Image from "react-bootstrap/Image";
 import { Button } from "react-bootstrap";
-
-import type { TProductItem } from "../../types/types";
+import { Fragment } from "react";
+import type { TSidebarCartItem } from "@/types/types";
+import styles from "@/styles/SidebarCart.module.scss";
+import { AiOutlineTags } from "react-icons/ai";
 
 export default function CartItem({
-  product: { slug, image, name, databaseId },
-  price,
+  product: { databaseId, image, name, price, slug },
   handleRemoveItem,
   couponApplied,
-  totalProductsPrice,
-}: TProductItem) {
+  couponAmount,
+  couponCode,
+}: TSidebarCartItem) {
+  const couponPrice = price - couponAmount;
+
   return (
-    <div className={styles.Checkout__product}>
-      <div className={styles.Checkout_topImg}>
-        <Image src={image} width="71" height="71" alt="Cart items" priority />
+    <div data-testid="cart-item" className={styles.BBP__SidebarCart_product}>
+      <div className={styles.BBP__SidebarCart_Image}>
+        <Image src={image} width="71" height="71" alt="Cart items" />
       </div>
-      <div className={styles.Checkout__right}>
+      <div className={styles.BBP__SidebarCart_right}>
         <h3>
           <Link href={`/product/${slug}`} style={{ color: "#333" }}>
             {name}
           </Link>
 
           {!couponApplied ? (
-            <span className={styles.Checkout__right_price}>${price}</span>
+            <Fragment>
+              <span className={styles.BBP__SidebarCart_price}>${price}</span>
+            </Fragment>
           ) : (
-            <span className={styles.Checkout__right_price}>
-              ${totalProductsPrice}
-            </span>
+            <Fragment>
+              <span
+                className={styles.BBP__SidebarCart_price}
+                style={{ fontWeight: 100, textDecoration: "line-through" }}
+              >
+                ${price}
+              </span>
+              <span className={styles.BBP__SidebarCart_price}>
+                ${couponPrice}
+              </span>
+
+              {couponApplied && (
+                <Fragment>
+                  <AiOutlineTags fontSize={18} />
+                  <span
+                    style={{ fontSize: "0.7rem" }}
+                    className={styles.BBP__SidebarCart_Coupon_Text}
+                  >
+                    {couponCode} (-${couponAmount.toFixed(2)})
+                  </span>
+                </Fragment>
+              )}
+            </Fragment>
           )}
         </h3>
 
         <Button
           onClick={() => handleRemoveItem(databaseId)}
-          className={styles.SidebarCart__OffCanvas_button}
           variant="secondary"
         >
           Remove
