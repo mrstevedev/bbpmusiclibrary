c24dc2d03d0e83ba77fe0507b611b9b9bb758b7f
Steven <stevendotpulido@gmail.com>
2024-07-02 00:24:18 -0700

    Add YouTubeEmbed component

---
+++ src/components/YouTube/YouTubeEmbed.tsx
@@ -0,0 +1,37 @@
+import { TProductSoundcloud } from "@/types/types";
+import styles from "@/styles/Soundcloud.module.scss";
+
+export default function YouTubeEmbed({ product }: TProductSoundcloud) {
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height="300"
+        src="https://www.youtube.com/embed/UNecT1CUAGY?si=fXqXhMSzDknVh4rz"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <a
+        href="https://soundcloud.com/bbpmusiclibrary"
+        title="BBP Music Library"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.BBP_Soundcloud__Name}
+      >
+        BBP Music Library
+      </a>
+      ·
+      <a
+        href={product.name}
+        title={product.name}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.BBP_Soundcloud__ShortDescription}
+      >
+        {product.name}
+      </a>
+    </div>
+  );
+}
