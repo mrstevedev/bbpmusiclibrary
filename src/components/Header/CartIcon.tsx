3d17ebf5cc3d80b3247f44f9168bbb4c264a25eb
Steven <stevendotpulido@gmail.com>
2024-07-02 02:34:10 -0700

    Add Header

--- src/components/Header/CartIcon.tsx
+++ src/components/Header/CartIcon.tsx
@@ -8,24 +8,26 @@ type Props = {
 
 export default function CartIcon({ handleToggleCart, cartCount }: Props) {
   return (
-    <>
-      <a
-        onClick={handleToggleCart}
-        className="cart-icon"
-        data-effect="st-effect-1"
-      >
-        <span
-          className={`cart-count ${cartCount > 0 ? styles.cart__count : null}`}
-        ></span>
-        <Image
-          src={"/images/cart.svg"}
-          width="22"
-          height="21"
-          alt="shopping cart"
-          style={{ display: "flex" }}
-          className={styles.btn__hover_scale}
-        />
-      </a>
-    </>
+    <a
+      data-testid="cart-button"
+      onClick={handleToggleCart}
+      className="cart-icon"
+      data-effect="st-effect-1"
+    >
+      <span
+        data-testid={cartCount > 0 ? "has-items" : null}
+        className={`cart-count ${
+          cartCount > 0 ? styles.BBP_Cart__Count : null
+        }`}
+      ></span>
+      <Image
+        src={"/images/cart.svg"}
+        width="33"
+        height="32"
+        alt="shopping cart"
+        style={{ display: "flex" }}
+        className={styles.BBP_Button_Hover__Scale}
+      />
+    </a>
   );
 }
--- src/components/Header/HeaderWrapper.tsx
+++ src/components/Header/HeaderWrapper.tsx
@@ -2,7 +2,7 @@
 import Header from "./Header";
 import { Fragment } from "react";
 import { usePathname } from "next/navigation";
-import Breadcrumb from "src/components/Breadcrumb/Breadcrumb";
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
 
 export default function HeaderWrapper() {
   const path = usePathname();
@@ -12,12 +12,16 @@ export default function HeaderWrapper() {
       {path !== "/checkout" ? (
         <Fragment>
           <Header />
-          <Breadcrumb
-            homeElement={"Home"}
-            activeClasses="breadcrumb-active"
-            containerClasses="flex"
-            capitalizeLinks
-          />
+          {path !== "/" ? (
+            <Fragment>
+              <Breadcrumb
+                homeElement={"Home"}
+                activeClasses="breadcrumb-active"
+                containerClasses="flex"
+                capitalizeLinks
+              />
+            </Fragment>
+          ) : null}
         </Fragment>
       ) : null}
     </Fragment>
