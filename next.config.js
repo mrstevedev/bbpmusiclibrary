331160b6f24fb8d00e492858b335ddd63f00e71e
Steven <stevendotpulido@gmail.com>
2024-07-02 02:43:15 -0700

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "bbpmusiclib.wpenginepowered.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d1hx41nm7bdfp5.cloudfront.net",
       },
     ],
   },
