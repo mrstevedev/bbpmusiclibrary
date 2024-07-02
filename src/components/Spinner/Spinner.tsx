71880330411dd9db267232977239d4bf295e6ebc
Steven <stevendotpulido@gmail.com>
2024-07-02 02:44:01 -0700

    Add Spinner component

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Spinner.module.scss";
 export default function Spinner() {
   return (
     <>
-      <div className={`spinner-border ${styles.Spinner}`} role="status">
+      <div className={`spinner-border ${styles.BBP__Spinner}`} role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
     </>
