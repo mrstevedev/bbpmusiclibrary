16d731376cbb3b6ec84de113d621827181a92584
Steven <stevendotpulido@gmail.com>
2024-07-02 10:22:27 -0700

    Add checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import CheckoutWrapper from "@/components/Checkout/CheckoutWrapper";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "BBP Music Library | Bonita Basics Productions Music Library | Checkout",
+  description:
+    "About BBPMusicLibrary, Sample Curator, Digital download Sample Packs for Hip Hop, Boom Bap music production",
+};
+export default function page() {
+  return <CheckoutWrapper />;
+}
