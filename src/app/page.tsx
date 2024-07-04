900e7b09ab76ff5bb471b4e69a62bd68f3268503
Steven <stevendotpulido@gmail.com>
2024-07-03 17:22:31 -0700

    Add opengraoh to metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ export const metadata: Metadata = {
   title: "BBP Music Library | Bonita Basics Productions Music Library",
   description:
     "About BBPMusicLibrary, Sample Curator, Digital download Sample Packs for Hip Hop, Boom Bap music production",
+  openGraph: {
+    images:
+      "https://d1hx41nm7bdfp5.cloudfront.net/wp-content/uploads/2024/07/02091123/bbpmusiclib_silhouette.webp",
+  },
 };
 
 export default function Home() {
