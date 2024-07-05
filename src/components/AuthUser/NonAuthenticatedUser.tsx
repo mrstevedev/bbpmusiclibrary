2ce102db299fe40e065fdad90101de435763caef
Steven <stevendotpulido@gmail.com>
2024-07-05 15:20:18 -0700

    Move anchor to wrap Signin Button component

--- src/components/AuthUser/NonAuthenticatedUser.tsx
+++ src/components/AuthUser/NonAuthenticatedUser.tsx
@@ -8,14 +8,14 @@ export default function NonAuthenticatedUser() {
       <span className="nav-link" style={{ paddingLeft: 0 }}>
         Guest
       </span>
-      <Button
-        color="white"
-        style={{ borderRadius: "22px", padding: "0 1.3rem" }}
-      >
-        <Link href="/login" style={{ paddingLeft: 0 }}>
+      <Link href="/login" style={{ display: "flex", paddingLeft: 0 }}>
+        <Button
+          color="white"
+          style={{ borderRadius: "22px", padding: "0 1.3rem" }}
+        >
           <span style={{ color: "white" }}>Sign-In</span>
-        </Link>
-      </Button>
+        </Button>
+      </Link>
     </Fragment>
   );
 }
