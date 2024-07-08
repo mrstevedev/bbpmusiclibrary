ccc2d219031d81dd5b575e427e3b01bfb065b69f
Steven <stevendotpulido@gmail.com>
2024-07-07 23:35:22 -0700

    Update to USER enum

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { USER } from "@/constants/index";
 
 export async function GET(request: Request, response: Response) {
-  cookies().delete("bbp_user");
+  cookies().delete(USER.BBP_USER);
   cookies().delete("bbp_token");
   cookies().delete("bbp_customer_id");
-  return NextResponse.json("Handle logout in route handler", { status: 200 });
+  return NextResponse.json("User logged out", { status: 200 });
 }
