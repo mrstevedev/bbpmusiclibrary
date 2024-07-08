24b8444fcaa00a6e65c6afce0b8de91dd7a11862
Steven <stevendotpulido@gmail.com>
2024-07-08 00:52:43 -0700

    Add USER enum

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
-import { USER } from "./constants";
+import { USER } from "@/constants/index";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const cookieStore = cookies();
-  const user = cookieStore.get(USER);
+  const user = cookieStore.get(USER.BBP_USER);
 
   if (!user) {
     return NextResponse.redirect(new URL("/", request.url));
