40943edba8ec9acd46e6cdb27c0444b107ffc335
Steven <stevendotpulido@gmail.com>
2024-07-17 18:13:32 -0700

    Added SingleItemBuyNowButton button component

---
+++ src/components/Buttons/SingleItemBuyNowButton.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { ROUTE } from "@/constants/index";
+import Link from "next/link";
+import styles from "@/styles/SingleItemBuyNowButton.module.scss";
+
+export default function SingleItemBuyNowButton({
+  item,
+  cart,
+  handleAddSingleItemToCart,
+}) {
+  const isMatched = cart?.products?.some(
+    (data) => data.databaseId === item.databaseId
+  );
+
+  return (
+    <>
+      {isMatched ? (
+        <>
+          <Link href={ROUTE.CHECKOUT}>
+            <Button
+              variant="sm"
+              className={styles.SingleItemBuyNowButtonSelected}
+            >
+              In checkout
+            </Button>
+          </Link>
+        </>
+      ) : (
+        <Button
+          variant="sm"
+          className={styles.SingleItemBuyNowButton}
+          onClick={() => handleAddSingleItemToCart(item)}
+        >
+          Buy
+        </Button>
+      )}
+    </>
+  );
+}
