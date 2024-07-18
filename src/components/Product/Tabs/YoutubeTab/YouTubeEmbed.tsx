0abb18f96cb88b16a780418c71b16d6a7a720ddb
Steven <stevendotpulido@gmail.com>
2024-07-17 22:28:52 -0700

    Move Souncloud Youtube & Downloads components

---
+++ src/components/Product/Tabs/YoutubeTab/YouTubeEmbed.tsx
@@ -0,0 +1,39 @@
+import { TProductSoundcloud } from "@/types/types";
+import styles from "@/styles/Soundcloud.module.scss";
+
+export default function YouTubeEmbed({ product }: TProductSoundcloud) {
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height="300"
+        src={`https://www.youtube.com/embed/${
+          product.attributes ? product.attributes.nodes[0].options : null
+        }`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <a
+        href="https://soundcloud.com/bbpmusiclibrary"
+        title="BBP Music Library"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.BBP_Soundcloud__Name}
+      >
+        BBP Music Library
+      </a>
+      ·
+      <a
+        href={product.name}
+        title={product.name}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.BBP_Soundcloud__ShortDescription}
+      >
+        {product.name}
+      </a>
+    </div>
+  );
+}
