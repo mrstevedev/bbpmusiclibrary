c6d63ceeee82f8f294dcb5670262abb3c84f8897
Steven <stevendotpulido@gmail.com>
2024-07-18 00:00:21 -0700

    Update button text

--- src/components/Buttons/AddToCartButton.tsx
+++ src/components/Buttons/AddToCartButton.tsx
@@ -27,7 +27,7 @@ export default function AddToCartButton({
                 style={{ cursor: "pointer" }}
               >
                 <span className={styles.BBP__AddToCart_Button_Text}>
-                  View in checkout{" "}
+                  In checkout{" "}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="28"
