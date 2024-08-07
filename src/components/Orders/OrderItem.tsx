e9de34ca518663f4fb4addacebd223e73b294955
Steven <stevendotpulido@gmail.com>
2024-08-07 16:28:10 -0700

    Refactor markup and separate styles

--- src/components/Orders/OrderItem.tsx
+++ src/components/Orders/OrderItem.tsx
@@ -1,61 +1,38 @@
 "use client";
 import Image from "next/image";
 import Nav from "react-bootstrap/Nav";
-import { Fragment } from "react";
 import { format } from "date-fns";
+import styles from "@/styles/Orders.module.scss";
 
 export default function OrderItem({ orderNumber, date, lineItems }) {
   return (
-    <Nav.Item as="li" style={{ margin: "1rem 0" }}>
+    <Nav.Item as="li">
       {lineItems.map((data) => {
         return (
-          <div
-            key={data.product.databaseId}
-            style={{ display: "flex", margin: "1rem 0" }}
-          >
+          <div key={data.product.databaseId} className={styles.BBP__OrderItems}>
             <Image
-              className="Orders__image"
-              src={data.product.image.mediaItemUrl}
               width={200}
               height={200}
               alt={data.product.name}
-              style={{ borderRadius: "4px" }}
+              src={data.product.image.mediaItemUrl}
+              className={styles.BBP__OrderItem_Image}
             />
 
-            <Nav
-              as="ul"
-              style={{
-                display: "block",
-                padding: "0px 1rem",
-                listStyle: "none",
-                fontWeight: 100,
-                lineHeight: "1.15",
-              }}
-            >
-              <Fragment>
-                <Nav.Item>
-                  <strong>Order ID</strong>
-                  <div>
-                    <p>{orderNumber}</p>
-                  </div>
-                </Nav.Item>
-                <Nav.Item>
-                  <strong>Filename</strong>
-                  <div>
-                    <p>{data.product.name}</p>
-                  </div>
-                </Nav.Item>
-                <Nav.Item>
-                  <strong>Price</strong>
-                  <div>
-                    <p>{data.product.price}</p>
-                  </div>
-                </Nav.Item>
-              </Fragment>
+            <Nav as="ul" className={styles.BBP__OrderItem_List}>
               <Nav.Item>
-                <div>
-                  <strong>Purchased</strong>
-                </div>
+                <strong>Order ID</strong>
+                <p>{orderNumber}</p>
+              </Nav.Item>
+              <Nav.Item>
+                <strong>Filename</strong>
+                <p>{data.product.name}</p>
+              </Nav.Item>
+              <Nav.Item>
+                <strong>Price</strong>
+                <p>{data.product.price}</p>
+              </Nav.Item>
+              <Nav.Item>
+                <strong>Purchased</strong>
                 <p>{format(new Date(date), "LLL. dd, yyyy")}</p>
               </Nav.Item>
             </Nav>
