6c0dd66701e65d2f2f6a2cf55308a55898907f06
Steven <stevendotpulido@gmail.com>
2024-08-07 22:27:43 -0700

    Use useLocale hook to navigate to current locale

--- src/components/AuthUser/NonAuthenticatedUser.tsx
+++ src/components/AuthUser/NonAuthenticatedUser.tsx
@@ -1,17 +1,22 @@
 import Link from "next/link";
 import { Fragment } from "react";
 import { Button } from "react-bootstrap";
+import { useLocale } from "next-intl";
 
 export default function NonAuthenticatedUser() {
+  const locale = useLocale();
   return (
     <Fragment>
       <span className="nav-link" style={{ paddingLeft: 0 }}>
         Guest
       </span>
-      <Link href="/login" style={{ display: "flex", paddingLeft: 0 }}>
+      <Link
+        href={`/${locale}/login`}
+        style={{ display: "flex", paddingLeft: 0 }}
+      >
         <Button
           color="white"
-          style={{ borderRadius: "8px", padding: "0.3rem 1.1rem" }}
+          style={{ borderRadius: "6px", padding: "0.3rem 1.1rem" }}
         >
           <span style={{ color: "white" }}>Sign-In</span>
         </Button>
