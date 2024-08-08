6b73155875b63d0082a2579e0591b1703979db39
Steven <stevendotpulido@gmail.com>
2024-08-07 17:22:55 -0700

    Update next.config.js with next-intl plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  reactStrictMode: false,
+// @ts-check
+const createNextIntlPlugin = require("next-intl/plugin");
+const withNextIntl = createNextIntlPlugin();
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
@@ -11,6 +16,17 @@ module.exports = {
         protocol: "https",
         hostname: "d1hx41nm7bdfp5.cloudfront.net",
       },
+      {
+        protocol: "https",
+        hostname: "flagicons.lipis.dev",
+      },
     ],
   },
+  i18n: {
+    locales: ["en", "de", "fr", "sr", "es"],
+    defaultLocale: "en",
+  },
 };
+nextConfig.i18n = undefined;
+
+module.exports = withNextIntl(nextConfig);
