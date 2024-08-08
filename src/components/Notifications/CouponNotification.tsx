208ea8e8914f36f5ddcba55f7c0c15b7a6ec7429
Steven <stevendotpulido@gmail.com>
2024-08-07 23:10:18 -0700

    Add translation

--- src/components/Notifications/CouponNotification.tsx
+++ src/components/Notifications/CouponNotification.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { Alert } from "react-bootstrap";
+import { useTranslations } from "next-intl";
 import { Fragment, useContext } from "react";
 import styles from "@/styles/CouponNotification.module.scss";
 import { CouponContext, TCouponContext } from "@/context/CouponContext";
+import { TRANSLATE } from "@/constants/index";
 
 export default function CouponNotification() {
   const { coupon } = useContext<TCouponContext>(CouponContext);
   const description = coupon?.description;
+  const t = useTranslations(TRANSLATE.TRNASLATE_COUPON_NOTIFICATION);
 
   return (
     <Fragment>
       {coupon ? (
-        <Alert className={`alert-fade ${styles.BBP_Coupon__Notification}`}>
-          {description}
+        <Alert className={`${styles.BBP_Coupon__Notification}`}>
+          {t("description")}
         </Alert>
       ) : null}
     </Fragment>
