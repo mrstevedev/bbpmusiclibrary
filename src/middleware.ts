030fc3f8e188b1afd895081beb7864648de300e1
Steven <stevendotpulido@gmail.com>
2024-08-08 03:12:47 -0700

    Update matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,7 +30,7 @@ const i18nMiddlewareFn = ({ request }: MiddlewareFunctionProps) => {
 };
 
 const middlewares = {
-  "/": i18nMiddlewareFn,
+  "/((?!api|_next|_vercel|.*\\..*).*)": i18nMiddlewareFn,
   "/(es|en|de|fr|se)": i18nMiddlewareFn,
   "/(es|en|de|fr|se|jp)/account": i18nMiddlewareFn,
   "/(es|en|de|fr|se|jp)/account/:path": i18nMiddlewareFn,
