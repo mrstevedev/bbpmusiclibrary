22833a60a967c39a32d4cfb2a6b6dcab6abe032f
Steven <stevendotpulido@gmail.com>
2024-08-09 13:31:31 -0700

    Add custom Link component for i18n navigation

---
+++ src/components/LangToggle/CustomI18nLink.tsx
@@ -0,0 +1,26 @@
+import { AppConfig } from "@/util/AppConfig";
+import Link from "next/link";
+
+type CustomI18nLinkProps = {
+  href: string;
+  locale: string;
+  children: React.ReactNode;
+  [key: string]: any;
+};
+
+export default function CustomI18nLink({
+  href,
+  locale,
+  children,
+  ...props
+}: CustomI18nLinkProps) {
+  const isDefaultLocale = locale === AppConfig.defaultLocale;
+  const path = isDefaultLocale ? href : `/${locale}${href}`;
+
+  console.log(href, `/${locale}${href}`);
+  return (
+    <Link href={path} {...props} className="link" title={props.name}>
+      {children}
+    </Link>
+  );
+}
