29a6569443ef1c107d9ae2f8022ca9c5aa26a38e
Steven <stevendotpulido@gmail.com>
2024-08-11 20:21:50 -0700

    Add custom i18n link

--- src/components/Buttons/ContinueShoppingButton.tsx
+++ src/components/Buttons/ContinueShoppingButton.tsx
@@ -1,18 +1,22 @@
 import styles from "@/styles/SidebarCart.module.scss";
 import Link from "next/link";
 import { Button } from "react-bootstrap";
+import { useLocale } from "next-intl";
+import CustomI18nLink from "../LangToggle/CustomI18nLink";
 
 export const ContinueShoppingButton = ({ handleCloseCart }) => {
+  const locale = useLocale();
+
   return (
     <>
-      <Link href="/" passHref>
+      <CustomI18nLink href="/checkout" locale={locale} passHref>
         <Button
           onClick={handleCloseCart}
           className={`btn btn-primary rounded-2 ${styles.BBP__SidebarCart_button}`}
         >
           Continue shopping
         </Button>
-      </Link>
+      </CustomI18nLink>
     </>
   );
 };
