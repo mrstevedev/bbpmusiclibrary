9e064302f5cf4e74dfd487c17969a00cff664c84
Steven <stevendotpulido@gmail.com>
2024-08-11 22:06:13 -0700

    Remove custom i18n link

--- src/components/Buttons/ContinueShoppingButton.tsx
+++ src/components/Buttons/ContinueShoppingButton.tsx
@@ -1,22 +1,19 @@
 import styles from "@/styles/SidebarCart.module.scss";
 import Link from "next/link";
 import { Button } from "react-bootstrap";
-import { useLocale } from "next-intl";
 import CustomI18nLink from "../LangToggle/CustomI18nLink";
 
 export const ContinueShoppingButton = ({ handleCloseCart }) => {
-  const locale = useLocale();
-
   return (
     <>
-      <CustomI18nLink href="/checkout" locale={locale} passHref>
+      <Link href="/" passHref>
         <Button
           onClick={handleCloseCart}
           className={`btn btn-primary rounded-2 ${styles.BBP__SidebarCart_button}`}
         >
           Continue shopping
         </Button>
-      </CustomI18nLink>
+      </Link>
     </>
   );
 };
