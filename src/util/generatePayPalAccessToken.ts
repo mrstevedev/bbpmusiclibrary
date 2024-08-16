f883603d4aaa05665a8d18e7535015da1b6d6598
Steven <stevendotpulido@gmail.com>
2024-08-16 12:56:18 -0700

    Added use server directive

--- src/util/generatePayPalAccessToken.ts
+++ src/util/generatePayPalAccessToken.ts
@@ -1,3 +1,4 @@
+"use server";
 import axios, { AxiosError } from "axios";
 import qs from "querystring";
 
@@ -8,19 +9,14 @@ export const generatePayPalAccessToken = async () => {
 
   const clientID = process.env.PAYPAL_CLIENT_ID;
   const secretKEY = process.env.PAYPAL_SECRET_KEY;
-
   const PAYPAL_AUTH_URL = process.env.PAYPAL_AUTH_URL;
-
   const data = qs.stringify({
     grant_type: "client_credentials",
   });
-
   // "grant_type=client_credentials"
-
   // Base64 Encode ClientID:ClientSecret
   const stringToEncode = clientID + ":" + secretKEY;
   const base64Encoded = btoa(stringToEncode);
-
   const config = {
     method: "POST",
     url: PAYPAL_AUTH_URL,
@@ -30,7 +26,7 @@ export const generatePayPalAccessToken = async () => {
     },
     data: data,
   };
-
   const response = await axios.request(config);
+  // console.log("response for access_token:::", response);
   return response.data.access_token;
 };
