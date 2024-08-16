b71e8476ee558f91719b8747f22dcaa6c4fdd823
Steven <stevendotpulido@gmail.com>
2024-08-16 12:55:58 -0700

    Added sandbox to options

--- src/providers/PayPalScriptProvider.tsx
+++ src/providers/PayPalScriptProvider.tsx
@@ -2,16 +2,16 @@
 
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const initialOptions = {
-  clientId:
-    "AUO_v6th3x2ZiCyv7XRw3eMpd5u7W-51NE04Z6LCiNPd4E-k58dw_rTcCekgND-zdgTmfuQaKpGx6O_X",
-  currency: "USD",
-  intent: "capture",
-};
-
 export default function PayPalButtonScriptProvider({ children }) {
   return (
-    <PayPalScriptProvider options={initialOptions}>
+    <PayPalScriptProvider
+      options={{
+        merchantId: "Q9DCN996UBRWG",
+        clientId: "test",
+        intent: "capture",
+        currency: "USD",
+      }}
+    >
       {children}
     </PayPalScriptProvider>
   );
