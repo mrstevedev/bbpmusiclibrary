a839f900c2aec99996e8b633abcbfae166855821
Steven <stevendotpulido@gmail.com>
2024-08-16 14:39:41 -0700

    Refactored to axios added /capture in place of /create route

--- src/components/Forms/Checkout/Button/PayPalButtonCheckout.tsx
+++ src/components/Forms/Checkout/Button/PayPalButtonCheckout.tsx
@@ -6,6 +6,8 @@ import { toast } from "react-toastify";
 import { delay, formatPhoneNumber } from "@/util/index";
 import { CouponContext, TCouponContext } from "@/context/CouponContext";
 import { ROUTE, METHOD, CART } from "@/constants/index";
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+import PayPalButtonScriptProvider from "@/providers/PayPalScriptProvider";
 
 export default function PayPalButtonCheckout({
   values,
@@ -39,110 +41,60 @@ export default function PayPalButtonCheckout({
 
   console.log(values);
   const { coupon } = useContext<TCouponContext>(CouponContext);
-  const style = { height: 55, color: "blue", label: "pay" };
+
+  const handleCreateOrder = async () => {
+    for (const val in values) {
+      if (values[val] === "") {
+        await delay(2000);
+        handleSubmit();
+        return;
+      }
+    }
+
+    const orderUrl = process.env.NEXT_PUBLIC_API_URL + ROUTE.ORDER;
+
+    const payload = JSON.stringify({
+      purchase_units: purchaseUnits,
+    });
+
+    const orderResponse = await axios.post(orderUrl, payload, {
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await orderResponse.data;
+    return data.id;
+  };
+
+  const handleOnAppove = async (data): Promise<void> => {
+    const captureUrl = process.env.NEXT_PUBLIC_API_URL + ROUTE.CAPTURE;
+    const productIds = purchaseUnits.map((data) => data.reference_id);
+
+    const payload = JSON.stringify({
+      id: data.orderID,
+      productIds,
+      coupon: coupon?.isApplied ? coupon.code : null,
+    });
+
+    try {
+      await axios.post(captureUrl, payload, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      }
+    }
+  };
+
   return (
-    <>Paypal Button js here</>
-    // <PayPalButton
-    //   style={style}
-    //   options={{
-    //     // Live merchantId
-    //     // merchantId: "IDRP5GEJ9Q6B4NG"
-    //     merchantId: "SVNDVQ7UVZEBE",
-    //     // Live Credentials for REST API
-    //     // clientId: "AcKBIn299K6i0Q4_bOV1JgxcSphc475Kr-yk3Oon45JP_NuzUi_b364xkOq7icivXgr4tbH2O-urvdvk"
-    //     // secret: EI-gxMRCYQqfGFPAGZUrKowjnAK9c-vigewSTCK8cgZV_9kg6NuT9rSOSiRXgCA9BfWJ9McjB0VOZNvh
-    //     clientId: "sb",
-    //     currency: "USD",
-    //     disableFunding: "venmo,card,credit,ideal,sofort,sepa,giropay",
-    //   }}
-    //   createOrder={async (data: any, actions: any) => {
-    //     for (const val in values) {
-    //       if (values[val] === "") {
-    //         await delay(2000);
-    //         handleSubmit();
-    //         return;
-    //       }
-    //       return actions.order.create({
-    //         purchase_units: purchaseUnits,
-    //         application_context: {
-    //           shipping_preferences: "NO_SHIPPING",
-    //         },
-    //       });
-    //     }
-    //   }}
-    //   onSuccess={async (details: any) => {
-    //     const email = details.payer.email_address;
-    //     const address = details.payer.address.address_line_1;
-    //     const admin_area_1 = details.payer.address.admin_area_1;
-    //     const admin_area_2 = details.payer.address.admin_area_2;
-    //     const postal_code = details.payer.address.postal_code;
-    //     const country_code = details.payer.address.country_code;
-    //     const phone = formatPhoneNumber(
-    //       details.payer.phone.phone_number.national_number
-    //     );
-    //     const id = details.id;
-    //     const first_name = details.payer.name.given_name;
-    //     const last_name = details.payer.name.surname;
-    //     const product_name = details.purchase_units[0].description;
-    //     const purchase_units = details.purchase_units;
-
-    //     const data = JSON.stringify({
-    //       id: id,
-    //       email: email,
-    //       phone: phone,
-    //       payment_method: METHOD.METHOD_PAYPAL,
-    //       admin_area_1: admin_area_1,
-    //       admin_area_2: admin_area_2,
-    //       postal_code: postal_code,
-    //       country_code: country_code,
-    //       product_name: product_name,
-    //       purchaseUnits: purchase_units,
-    //       address: address,
-    //       name: first_name + " " + last_name,
-    //       coupon: coupon?.isApplied ? coupon.code : null,
-    //     });
-    //     console.log("details", details);
-
-    //     try {
-    //       const response = await axios.post(
-    //         process.env.NEXT_PUBLIC_API_URL + ROUTE.CREATE,
-    //         data,
-    //         {
-    //           headers: {
-    //             "Content-Type": "application/json",
-    //           },
-    //         }
-    //       );
-
-    //       if (response.data) {
-    //         router.push(
-    //           "/confirm" +
-    //             "?" +
-    //             createQueryString(
-    //               "success",
-    //               "true",
-    //               "email",
-    //               email,
-    //               "orderID",
-    //               response.data.orderId,
-    //               "transactionID",
-    //               response.data.transactionId
-    //             )
-    //         );
-    //       }
-    //     } catch (error: unknown) {
-    //       if (error instanceof AxiosError) {
-    //         toast.error(error.response?.data.message);
-    //       }
-    //     }
-    //   }}
-    //   onError={(error: unknown) => {
-    //     if (error instanceof Error) {
-    //       if (purchaseUnits.length > 10) {
-    //         toast.error(CART.CART_INVALID_MAX_ITEMS);
-    //       }
-    //     }
-    //   }}
-    // />
+    <PayPalButtonScriptProvider>
+      <PayPalButtons
+        fundingSource="paypal"
+        style={{ height: 55, color: "blue", layout: "horizontal" }}
+        createOrder={handleCreateOrder}
+        onApprove={handleOnAppove}
+      />
+    </PayPalButtonScriptProvider>
   );
 }
